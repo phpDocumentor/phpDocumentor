deb06c189e69e1d6c63ea8efb0041bd9a13842ef
Mike van Riel <mike.vanriel@naenius.com>
2011-02-26 15:29:27 +0100

    Updated version number

--- DocBlox/Abstract.php
+++ DocBlox/Abstract.php
@@ -23,7 +23,7 @@ abstract class DocBlox_Abstract
    *
    * @var int
    */
-  const VERSION = '0.8.2';
+  const VERSION = '0.8.3';
 
   /**
    * The logger used to capture all messages send by the log method.
