d4050ed64d718ebaed2dc5bffff75808b26a74f2
Mike van Riel <mike.vanriel@naenius.com>
2011-03-06 09:34:19 +0100

    Fixed last tests for DocBlox_Log

--- test/unit/DocBlox/LogTest.php
+++ test/unit/DocBlox/LogTest.php
@@ -31,12 +31,45 @@ class DocBlox_LogTest extends PHPUnit_Framework_TestCase
 
   public function testThreshold()
   {
-    $this->markTestIncomplete();
+    $this->fixture->setThreshold(DocBlox_Log::ALERT);
+    $this->assertEquals(DocBlox_Log::ALERT, $this->fixture->getThreshold());
+
+    $this->fixture->setThreshold('crit');
+    $this->assertEquals(DocBlox_Log::CRIT, $this->fixture->getThreshold());
+
+    $this->setExpectedException('InvalidArgumentException');
+    $this->fixture->setThreshold('crit2');
   }
 
   public function testLog()
   {
-    $this->markTestIncomplete();
+    if (file_exists('/tmp/docblox_log_test'))
+    {
+      unlink('/tmp/docblox_log_test');
+    }
+
+    $this->fixture = new DocBlox_Log('/tmp/docblox_log_test');
+    $this->fixture->setThreshold(DocBlox_Log::ERR);
+    $this->fixture->log('test', DocBlox_Log::ERR);
+    $this->fixture->log('test2', DocBlox_Log::INFO);
+    $result = file_get_contents('/tmp/docblox_log_test');
+
+    $this->assertNotEmpty($result);
+    $this->assertContains('test', $result);
+    $this->assertNotContains('mb]:', $result, 'Should not contain debug information');
+    $this->assertNotContains('test2', $result, 'Should not contain test2 as it is of a lower level');
+
+    $this->fixture->setThreshold(DocBlox_Log::DEBUG);
+    $this->fixture->log('test3', DocBlox_Log::INFO);
+    $result = file_get_contents('/tmp/docblox_log_test');
+
+    $this->assertContains('test3', $result);
+    $this->assertContains('mb]:', $result, 'Should contain debug information when threshold is DEBUG');
+
+    $this->fixture->log(array('test4'), DocBlox_Log::INFO);
+    $result = file_get_contents('/tmp/docblox_log_test');
+
+    $this->assertContains('array', $result, 'The log should contain a var_dumped output');
   }
 
 }
