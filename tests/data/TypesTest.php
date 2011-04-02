9e68dba5f3cf20efcb3313b4ec007a5f80a7bbe1
Mike van Riel <mike.vanriel@naenius.com>
2011-04-02 16:11:30 +0200

    Updating TODO and creating and renaming test fixtures

---
+++ tests/data/TypesTest.php
@@ -0,0 +1,21 @@
+<?php
+include('NamespaceTest.php');
+include('../../src/DocBlox/Token.php');
+
+class TypesTest
+{
+
+  /**
+   * @param string $a
+   * @param null   $b
+   * @param array  $c
+   * @param bool   $d
+   * @param bool   $e
+   *
+   * @return void
+   */
+  public function test($a = 'null', $b = null, $c = array(), $d = true, $e = false)
+  {
+
+  }
+}
