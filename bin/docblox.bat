42874695eb803172920b300d7c548ecf26d9493f
Mike van Riel <mike.vanriel@naenius.com>
2011-06-06 17:02:02 +0200

    fixed variable replacement error in batch file

--- bin/docblox.bat
+++ bin/docblox.bat
@@ -1,8 +1,8 @@
-@echo off 
+@echo off
 if "%PHPBIN%" == "" set PHPBIN=php.exe
 if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
 GOTO RUN
-:USE_PEAR_PATH 
+:USE_PEAR_PATH
 set PHPBIN=%PHP_PEAR_PHP_BIN%
-:RUN 
-"%PHPBIN%" "@bin_dir@\docblox" %*
+:RUN
+"%PHPBIN%" "@php_bin@\docblox" %*
