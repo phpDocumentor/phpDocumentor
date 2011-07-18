845414defde45b248389fb3333352e05f318e87e
Mike van Riel <mike.vanriel@naenius.com>
2011-07-18 16:57:28 +0200

    GH #154: fixed errors when using special (xml) characters in the long description or todo description

--- src/DocBlox/Transformer/Behaviour/Inherit/Node/Abstract.php
+++ src/DocBlox/Transformer/Behaviour/Inherit/Node/Abstract.php
@@ -172,10 +172,12 @@ abstract class DocBlox_Transformer_Behaviour_Inherit_Node_Abstract
         } elseif ($desc && $super_desc) {
             // if a long description exists in both child and parent; insert the
             // parent's LD when the inline tag {@inheritdoc} is used.
-            $desc->nodeValue = str_ireplace(
-                '{@inheritdoc}',
-                $super_desc->nodeValue,
-                $desc->nodeValue
+            $desc->nodeValue = htmlspecialchars(
+                str_ireplace(
+                    '{@inheritdoc}',
+                    $super_desc->nodeValue,
+                    $desc->nodeValue
+                )
             );
         }
     }
