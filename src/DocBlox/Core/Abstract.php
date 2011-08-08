6eb70eeebf27225486fb01d948114cc0f51e5682
Mike van Riel <mike.vanriel@naenius.com>
2011-08-08 11:31:55 +0200

    RELEASE: Updated version number to 0.13.0

--- src/DocBlox/Core/Abstract.php
+++ src/DocBlox/Core/Abstract.php
@@ -27,7 +27,7 @@
 abstract class DocBlox_Core_Abstract
 {
     /** @var string The actual version number of DocBlox. */
-    const VERSION = '0.13-DEV';
+    const VERSION = '0.13.0';
 
     /**
      * The logger used to capture all messages send by the log method.
