76bf4ddc01d9d70abd5c8aadbf35616dedd1b319
Ben Selby <benmatselby@gmail.com>
2011-10-13 17:07:26 +0100

    The ability to define in plugin.xml for Core whether or not tags are required/deprecated based on certain docblock elements or globally
    - GH-289

---
+++ src/DocBlox/Plugin/Core/Parser/DocBlock/Validator/Required.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * File contains the DocBlox_Plugin_Core_Parser_DocBlock_Validator_Required class
+ *
+ * PHP Version 5
+ *
+ * @category   DocBlox
+ * @package    Parser
+ * @subpackage DocBlock_Validators
+ * @author     Ben Selby <benmatselby@gmail.com>
+ * @author     Mike van Riel <mike.vanriel@naenius.com>
+ * @copyright  2010-2011 Mike van Riel / Naenius. (http://www.naenius.com)
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT
+ * @link       http://docblox-project.org
+ */
+/**
+ * This class is responsible for validating which tags are required
+ * as defined in Docblox/src/DocBlox/Plugin/Core/plugin.xml
+ *
+ * @category   DocBlox
+ * @package    Parser
+ * @subpackage DocBlock_Validators
+ * @author     Ben Selby <benmatselby@gmail.com>
+ * @author     Mike van Riel <mike.vanriel@naenius.com>
+ * @copyright  2010-2011 Mike van Riel / Naenius. (http://www.naenius.com)
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT
+ * @link       http://docblox-project.org
+ */
+class DocBlox_Plugin_Core_Parser_DocBlock_Validator_Required
+    extends DocBlox_Plugin_Core_Parser_DocBlock_Validator_Abstract
+{
+    /**
+     * Is the docblock valid based on the rules defined in plugin.xml
+     *
+     * <options>
+     *   <option name="deprecated">
+     *      <tag name="deprecated" />
+     *      <tag name="access" />
+     *   </option>
+     *   <option name="required">
+     *     <tag name="package">
+     *       <element>DocBlox_Reflection_File</element>
+     *       <element>DocBlox_Reflection_Class</element>
+     *     </tag>
+     *     <tag name="subpackage">
+     *       <element>DocBlox_Reflection_File</element>
+     *       <element>DocBlox_Reflection_Class</element>
+     *     </tag>
+     *   </option>
+     * </options>
+     *
+     * @see DocBlox_Plugin_Core_Parser_DocBlock_Validator_Abstract::isValid()
+     */
+    public function isValid()
+    {
+        $docType = get_class($this->_source);
+        if (isset($this->_options['required'][$docType])) {
+            $this->validateTags($docType);
+        } else if (isset($this->_options['required']['__ALL__'])) {
+            $this->validateTags('__ALL__');
+        }
+    }
+
+    /**
+     * Validate the tags based on the type of docblock being
+     * parsed etc
+     *
+     * @param string $key Access key to $this->_options['required']
+     *
+     * @return void
+     */
+    protected function validateTags($key)
+    {
+
+        foreach ($this->_options['required'][$key] as $tag) {
+
+            if (count($this->_docblock->getTagsByName($tag)) == 0) {
+
+                $this->logParserError(
+                    'CRITICAL', 'Not found required tag "' . $tag .'" in ' . $this->_entityName, $this->_lineNumber
+                );
+            }
+        }
+    }
+}
