8f26f86599c7b18fadf12baa191aa3a9028695d5
Mike van Riel <mike.vanriel@naenius.com>
2011-11-12 09:30:08 +0100

    Allowed for more margin of error to compensate for busy test environments

--- tests/unit/DocBlox/BidirectionalIteratorTest.php
+++ tests/unit/DocBlox/BidirectionalIteratorTest.php
@@ -178,7 +178,7 @@ class DocBlox_BidirectionalIteratorTest extends PHPUnit_Framework_TestCase
         $times[] = microtime(true) - $time;
     }
     $average = array_sum($times) / count($times);
-    $this->assertLessThan(0.0003, $average);
+    $this->assertLessThan(0.0005, $average);
   }
 
   /**
