533eac5402a9b09f038b68592a4abe45158647aa
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-03-25 00:17:10 +1030

    Raise exception for Issue #343 when an empty/malformed DOMDocument is supplied

--- src/phpDocumentor/Plugin/Core/Transformer/Writer/Xsl.php
+++ src/phpDocumentor/Plugin/Core/Transformer/Writer/Xsl.php
@@ -59,8 +59,14 @@ class phpDocumentor_Plugin_Core_Transformer_Writer_Xsl
 
         $proc = new XSLTProcessor();
         $proc->importStyleSheet($xsl);
+        if (empty($structure->documentElement)) {
+            throw new phpDocumentor_Transformer_Exception(
+                'Specified DOMDocument lacks documentElement, cannot transform'
+            );
+        }
+
         $proc->setParameter(
-            '', 'title', $structure->documentElement->getAttribute('title')
+             '', 'title', $structure->documentElement->getAttribute('title')
         );
         $proc->setParameter(
             '', 'root',
