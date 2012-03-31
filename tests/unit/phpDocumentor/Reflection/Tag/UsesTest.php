625e6442dfb9c1a8a041bfb55b44187502c35743
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-03-31 22:22:44 +1030

    Unit test coverage

---
+++ tests/unit/phpDocumentor/Reflection/Tag/UsesTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * phpDocumentor Uses Tag Test
+ *
+ * @category   phpDocumentor
+ * @package    Reflection
+ * @subpackage Tests
+ * @author     Daniel O'Connor <daniel.oconnor@gmail.com>
+ * @copyright  Copyright (c) 2010-2011 Mike van Riel / Naenius. (http://www.naenius.com)
+ */
+
+/**
+ * Test class for phpDocumentor_Reflection_DocBlock_Tag_Uses
+ *
+ * @category   phpDocumentor
+ * @package    Reflection
+ * @subpackage Tests
+ * @author     Daniel O'Connor <daniel.oconnor@gmail.com>
+ * @copyright  Copyright (c) 2010-2011 Mike van Riel / Naenius. (http://www.naenius.com)
+ */
+class phpDocumentor_Reflection_DocBlock_Tag_UsesTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that the phpDocumentor_Reflection_DocBlock_Tag_Uses can create a link
+     * for the @uses doc block
+     *
+     * @param string $type
+     * @param string $content
+     * @param string $exName
+     * @param string $exContent
+     * @param string $exReference
+     *
+     * @covers phpDocumentor_Reflection_DocBlock_Tag_Uses::__construct
+     * @dataProvider provideDataForConstuctor
+     *
+     * @return void
+     */
+    public function testConstructorParesInputsIntoCorrectFields(
+        $type, $content, $exName, $exContent, $exDescription, $exReference
+    )
+    {
+        $tag = new phpDocumentor_Reflection_DocBlock_Tag_Uses($type, $content);
+
+        $actualName = $tag->getName();
+        $actualContent = $tag->getContent();
+        $actualDescription = $tag->getDescription();
+        $actualReference = $tag->getReference();
+
+        $this->assertEquals($exName, $actualName);
+        $this->assertEquals($exContent, $actualContent);
+        $this->assertEquals($exDescription, $actualDescription);
+        $this->assertEquals($exReference, $actualReference);
+    }
+
+    /**
+     * Data provider for testConstructorParesInputsIntoCorrectFields
+     *
+     * @return array
+     */
+    public function provideDataForConstuctor()
+    {
+        // $type, $content, $exName, $exContent, $exDescription, $exReference
+        return array(
+            array(
+                'uses',
+                'Foo::bar()',
+                'uses',
+                'Foo::bar()',
+                '',
+                'Foo::bar()'
+            ),
+            array(
+                'uses',
+                'Foo::bar() Testing',
+                'uses',
+                'Foo::bar() Testing',
+                'Testing',
+                'Foo::bar()',
+            ),
+            array(
+                'uses',
+                'Foo::bar() Testing comments',
+                'uses',
+                'Foo::bar() Testing comments',
+                'Testing comments',
+                'Foo::bar()',
+            ),
+        );
+    }
+}
