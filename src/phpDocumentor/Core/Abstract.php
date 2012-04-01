a3d3abf9f57d10682393a709325590c23bde836e
Mike van Riel <mike.vanriel@naenius.com>
2012-04-01 10:49:31 +0200

    RELEASE: Updated version number to 2.0.0a2

--- src/phpDocumentor/Core/Abstract.php
+++ src/phpDocumentor/Core/Abstract.php
@@ -27,7 +27,7 @@
 abstract class phpDocumentor_Core_Abstract
 {
     /** @var string The actual version number of phpDocumentor. */
-    const VERSION = '2.0.0-alpha2';
+    const VERSION = '2.0.0a2';
 
     /**
      * The logger used to capture all messages send by the log method.
