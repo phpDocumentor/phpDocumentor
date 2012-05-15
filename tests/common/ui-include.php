c866cbffde894a4bb68ea9f6a2012e208d1cf1c5
Mike van Riel <mike.vanriel@naenius.com>
2012-05-15 19:28:36 +0200

    Made bootstrap for ui tests compatible with new changes

--- tests/common/ui-include.php
+++ tests/common/ui-include.php
@@ -1,39 +1,14 @@
 <?php
-// determine base include folder, if @php_bin@ contains @php_bin then we do not install via PEAR
-$base_include_folder = (strpos('@php_dir@', '@php_dir') === 0)
-  ? dirname(__FILE__) . '/../../src'
-  : '@php_dir@/phpDocumentor/src';
-
-// set path to add lib folder, load the Zend Autoloader
-set_include_path($base_include_folder . PATH_SEPARATOR . get_include_path());
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5
+ *
+ * @author    Mike van Riel <mike.vanriel@naenius.com>
+ * @copyright 2010-2011 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
 
 require_once dirname(__FILE__) . '/../../src/phpDocumentor/Bootstrap.php';
-
-$autoloader = phpDocumentor_Bootstrap::createInstance()->registerAutoloader();
-
-$task_name = ($_SERVER['argc'] == 1) ? false : $_SERVER['argv'][1];
-$runner = new phpDocumentor_Task_Runner($task_name, 'project:run');
-$task = $runner->getTask();
-
-if (!$task->getQuiet() && (!$task->getProgressbar())) {
-    phpDocumentor_Core_Abstract::renderVersion();
-} else {
-    phpDocumentor_Core_Abstract::config()->logging->level = 'quiet';
-}
-if ($task->getVerbose()) {
-    phpDocumentor_Core_Abstract::config()->logging->level = 'debug';
-}
-
-// the plugins are registered here because the phpDocumentor_Task can load a
-// custom configuration; which is needed by this registration
-phpDocumentor_Bootstrap::createInstance()->registerPlugins($autoloader);
-
-try {
-    $task->execute();
-} catch (Exception $e) {
-    if (!$task->getQuiet()) {
-        echo 'ERROR: ' . $e->getMessage() . PHP_EOL . PHP_EOL;
-        echo $task->getUsageMessage();
-    }
-    die(1);
-}
+$autoloader = phpDocumentor_Bootstrap::createInstance()->initialize();
