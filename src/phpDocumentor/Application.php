625d6215a73f8899de17127fe16efb40ed3fffe9
Tony R Quilkey <thorpe@thorpesystems.com>
2012-09-11 19:12:02 +1000

    Fix paths to vendor autoloader.

--- src/phpDocumentor/Application.php
+++ src/phpDocumentor/Application.php
@@ -14,10 +14,10 @@ namespace phpDocumentor;
 
 use \Symfony\Component\Console\Input\InputInterface;
 
-if (file_exists('/../../vendor/autoload.php')) {
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
     require_once __DIR__ . '/../../vendor/autoload.php';
-} else if (file_exists('/../../../../../vendor/autoload.php')) {
-    require_once '/../../../../../vendor/autoload.php';
+} else if (file_exists(__DIR__ . '/../../../../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../../../../vendor/autoload.php';
 }
 
 /**
