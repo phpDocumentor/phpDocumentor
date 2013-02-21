4a6358dc0e1a3465acb4739ed676e3070e2ae91b
Mike van Riel <mike.vanriel@naenius.com>
2013-02-21 06:21:55 +0100

    Make Twig writer fully functional
    The Twig writer was not capable of querying the Objetc Graph and correctly creating
    artifacts. With this refactoring we introduced a Object Graph Walker that is capable of interpreting
    a light version of the twig syntax to get the correct element from an object graph.

--- src/phpDocumentor/Descriptor/Collection.php
+++ src/phpDocumentor/Descriptor/Collection.php
@@ -2,34 +2,68 @@
 
 namespace phpDocumentor\Descriptor;
 
-class Collection
+class Collection implements \Countable, \IteratorAggregate, \ArrayAccess
 {
+    /** @var mixed[] $items */
     protected $items = array();
 
+    /**
+     * Retrieve an external iterator.
+     *
+     * @return \Traversable
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->items);
+    }
+
+    /**
+     * Constructs a new collection object with optionally a series of Descriptors.
+     *
+     * @param mixed[] $items
+     */
     public function __construct($items = array())
     {
         $this->items = $items;
     }
 
+    /**
+     * Adds a new Descriptor.
+     *
+     * @param mixed $item
+     *
+     * @return void
+     */
     public function add($item)
     {
         $this->items[] = $item;
     }
 
+    /**
+     * Sets a new object onto the collection.
+     *
+     * @param string|integer $index
+     * @param mixed          $item
+     *
+     * @return void
+     */
     public function set($index, $item)
     {
-        $this->items[$index] = $item;
+        $this->offsetSet($index, $item);
     }
 
     public function get($index, $valueIfEmpty = null)
     {
-        if (!isset($this->items[$index])) {
-            $this->items[$index] = $valueIfEmpty;
+        if (!$this->offsetExists($index)) {
+            $this->offsetSet($index, $valueIfEmpty);
         }
 
-        return $this->items[$index];
+        return $this->offsetGet($index);
     }
 
+    /**
+     * @return mixed[]
+     */
     public function getAll()
     {
         return $this->items;
@@ -53,5 +87,54 @@ class Collection
     public function __get($name)
     {
         return $this->get($name);
+    }
+
+    /**
+     * Checks whether an item in this collection exists.
+     *
+     * @param string|integer $offset The index to check on.
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->items[$offset]);
+    }
+
+    /**
+     * Item to retrieve.
+     *
+     * @param string|integer $offset The offset to retrieve.
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return ($this->offsetExists($offset)) ? $this->items[$offset] : null;
+    }
+
+    /**
+     * Offset to set
+     *
+     * @param string|integer $offset The offset to assign the value to.
+     * @param mixed          $value  The value to set.
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->items[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @param string|integer $offset The offset to unset.
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
     }
 }
