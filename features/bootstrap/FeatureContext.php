32a8ca8872cc58f540f7a8827787c1ec45708673
Mike van Riel <mike.vanriel@naenius.com>
2013-05-24 23:06:47 +0200

    Separate feature context into separate subcontexts and add checkstyle scenarios

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -8,32 +8,14 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://phpdoc.org
  */
+
 use Behat\Behat\Context\BehatContext;
-use Behat\Gherkin\Node\PyStringNode;
 
 /**
  * Context class for the phpDocumentor Features.
  */
 class FeatureContext extends BehatContext
 {
-    /**
-     * Contains the last output of a iRun command.
-     *
-     * @see iRun() for the location where the variable is filled
-     *
-     * @var string
-     */
-    private $output;
-
-    /**
-     * Contains the return code of the last iRun command.
-     *
-     * @see iRun() for the location where the variable is filled
-     *
-     * @var int
-     */
-    private $return_code;
-
     /**
      * Initializes context.
      *
@@ -44,6 +26,16 @@ class FeatureContext extends BehatContext
      */
     public function __construct(array $parameters)
     {
+        $this->useContext('execution', new \ExecutionContext());
+        $this->useContext('checkstyle', new \CheckstyleContext());
+    }
+
+    public function getOutput()
+    {
+        /** @var \ExecutionContext $executionContext */
+        $executionContext = $this->getSubcontext('execution');
+
+        return $executionContext->getOutput();
     }
 
     /**
@@ -56,129 +48,6 @@ class FeatureContext extends BehatContext
     public function iAmInThePhpdocumentorRootDirectory()
     {
         chdir(__DIR__.'/../..');
-    }
-
-    /**
-     * Executes a command and sets the output and return code on this context.
-     *
-     * @param string $command The command to execute.
-     *
-     * @When /^I run "([^"]*)"$/
-     *
-     * @return void
-     */
-    public function iRun($command)
-    {
-        exec($command.' 2>&1', $this->output, $this->return_code);
-        $this->output = implode("\n", $this->output);
-    }
-
-    /**
-     * Execute a run of phpDocumentor without any files or folders.
-     *
-     * The configuration is explicitly disabled to prevent tainting via
-     * the configuration.
-     *
-     * @When /^I run phpDocumentor against no files or directories$/
-     *
-     * @return void
-     */
-    public function iRunPhpdocumentorAgainstNoFilesOrDirectories()
-    {
-        $this->iRun("php bin/phpdoc.php -t build --config=none --force");
-    }
-
-    /**
-     * Runs phpDocumentor with only the file that is provided in this command.
-     *
-     * The configuration is explicitly disabled to prevent tainting via
-     * the configuration.
-     *
-     * @param string $file_path
-     *
-     * @When /^I run phpDocumentor against the file "([^"]*)"$/
-     *
-     * @return void
-     */
-    public function iRunPhpDocumentorAgainstTheFile($file_path)
-    {
-        $this->iRun(
-            "php bin/phpdoc.php -f $file_path -t build --config=none --force"
-        );
-    }
-
-    /**
-     * Parses the given PHP code with phpDocumentor.
-     *
-     * The configuration is explicitly disabled to prevent tainting via
-     * the configuration.
-     *
-     * @param PyStringNode $code
-     *
-     * @When /^I run phpDocumentor with:$/
-     *
-     * @return void
-     */
-    public function iRunPhpDocumentorWith(PyStringNode $code)
-    {
-        $file = tempnam(sys_get_temp_dir(), 'pdb');
-        file_put_contents($file, $code);
-        $this->iRun("php bin/phpdoc.php -f $file -t build --config=none --force");
-        unlink($file);
-    }
-
-    /**
-     * Executes phpDocumentor and provides additional options.
-     *
-     * @param string $file_path
-     * @param string $options
-     *
-     * @When /^I run phpDocumentor against the file "([^"]*)" using option "([^"]*)"$/
-     *
-     * @return void
-     */
-    public function iRunPhpDocumentorAgainstTheFileUsingOption($file_path, $options)
-    {
-        $this->iRun(
-            "php bin/phpdoc.php -f $file_path -t build --config=none "
-            ."--force $options"
-        );
-    }
-
-    /**
-     * Executes phpDocumentor against the contents of a given folder.
-     *
-     * @param string $folder_path
-     *
-     * @When /^I run phpDocumentor against the directory "([^"]*)"$/
-     *
-     * @return void
-     */
-    public function iRunPhpDocumentorAgainstTheDirectory($folder_path)
-    {
-        $this->iRun(
-            "php bin/phpdoc.php -d $folder_path -t build --config=none --force"
-        );
-    }
-
-    /**
-     * Verifies whether the output of an iRun When is equal to the given.
-     *
-     * @param PyStringNode $string
-     *
-     * @Then /^I should get:$/
-     *
-     * @throws \Exception if the condition is not fulfilled
-     *
-     * @return void
-     */
-    public function iShouldGet(PyStringNode $string)
-    {
-        if ($this->output != trim($string->getRaw())) {
-            throw new \Exception(
-                "Actual output is:\n" . $this->output
-            );
-        }
     }
 
     /**
@@ -198,7 +67,7 @@ class FeatureContext extends BehatContext
     public function iShouldGetALogEntry($string)
     {
         $found = false;
-        foreach (explode("\n", $this->output) as $line) {
+        foreach (explode("\n", $this->getOutput()) as $line) {
             if (trim($line) == $string) {
                 $found = true;
             }
@@ -206,7 +75,7 @@ class FeatureContext extends BehatContext
 
         if (!$found) {
             throw new \Exception(
-                "Actual output is:\n" . $this->output
+                "Actual output is:\n" . $this->getOutput()
             );
         }
     }
@@ -225,7 +94,7 @@ class FeatureContext extends BehatContext
     public function iShouldNotGetALogEntry($string)
     {
         $found = false;
-        foreach (explode("\n", $this->output) as $line) {
+        foreach (explode("\n", $this->getOutput()) as $line) {
             if (trim($line) == $string) {
                 $found = true;
             }
@@ -233,7 +102,7 @@ class FeatureContext extends BehatContext
 
         if ($found) {
             throw new \Exception(
-                "Actual output is:\n" . $this->output
+                "Actual output is:\n" . $this->getOutput()
             );
         }
     }
@@ -252,7 +121,7 @@ class FeatureContext extends BehatContext
     public function iShouldGetALogEntryContaining($string)
     {
         $found = false;
-        foreach (explode("\n", $this->output) as $line) {
+        foreach (explode("\n", $this->getOutput()) as $line) {
             if (strpos(trim($line), $string) !== false) {
                 $found = true;
             }
@@ -260,7 +129,7 @@ class FeatureContext extends BehatContext
 
         if (!$found) {
             throw new \Exception(
-                "Actual output is:\n" . $this->output
+                "Actual output is:\n" . $this->getOutput()
             );
         }
     }
@@ -279,7 +148,7 @@ class FeatureContext extends BehatContext
     public function iShouldNotGetALogEntryContaining($string)
     {
         $found = false;
-        foreach (explode("\n", $this->output) as $line) {
+        foreach (explode("\n", $this->getOutput()) as $line) {
             if (strpos(trim($line), $string) !== false) {
                 $found = true;
             }
@@ -287,7 +156,7 @@ class FeatureContext extends BehatContext
 
         if ($found) {
             throw new \Exception(
-                "Actual output is:\n" . $this->output
+                "Actual output is:\n" . $this->getOutput()
             );
         }
     }
@@ -325,56 +194,5 @@ class FeatureContext extends BehatContext
     {
         $this->iShouldGetALogEntry('[Exception]');
         $this->iShouldGetALogEntryContaining($exception_text);
-    }
-
-    /**
-     * Verifies whether the return code was 0 and thus execution was a success.
-     *
-     * @Then /^the exit code should be zero$/
-     *
-     * @throws \Exception if the condition is not fulfilled
-     *
-     * @return void
-     */
-    public function theExitCodeShouldBeZero()
-    {
-        if ($this->return_code != 0) {
-            throw new \Exception(
-                'Return code was '.$this->return_code.' with output '
-                .$this->output
-            );
-        }
-    }
-
-    /**
-     * Verifies whether the return code was not null and it was not a success.
-     *
-     * @Then /^the exit code should be non-zero$/
-     *
-     * @throws \Exception if the condition is not fulfilled
-     *
-     * @return void
-     */
-    public function theExitCodeShouldBeNonZero()
-    {
-        if ($this->return_code == 0) {
-            throw new \Exception('Return code was 0');
-        }
-    }
-
-    /**
-     * Verifies whether nothing was written to STDOUT.
-     *
-     * @Then /^there should be no output$/
-     *
-     * @throws \Exception if the condition is not fulfilled
-     *
-     * @return void
-     */
-    public function thereShouldBeNoOutput()
-    {
-        if ($this->output != "") {
-            throw new \Exception('Output has been detected: '.$this->output);
-        }
     }
 }
