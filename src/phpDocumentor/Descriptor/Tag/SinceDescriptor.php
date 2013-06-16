bf4636efeee18f97684db1e3e9183e3b44a3cc77
Mike van Riel <mike.vanriel@naenius.com>
2013-06-16 22:30:22 +0200

    [874]: Inject version number with @version into template

---
+++ src/phpDocumentor/Descriptor/Tag/SinceDescriptor.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.3
+ *
+ * @copyright 2010-2013 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Descriptor\Tag;
+
+use phpDocumentor\Descriptor\TagDescriptor;
+use phpDocumentor\Reflection\DocBlock\Tag\SinceTag;
+
+class SinceDescriptor extends TagDescriptor
+{
+    protected $version;
+
+    public function __construct(SinceTag $reflectionTag)
+    {
+        parent::__construct($reflectionTag);
+
+        $this->version = $reflectionTag->getVersion();
+    }
+
+    public function getVersion()
+    {
+        return $this->version;
+    }
+}
