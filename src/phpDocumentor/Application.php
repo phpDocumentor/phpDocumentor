691bb3b8d5e77f947800a0f291e1b0918d5e3481
Siad Ardroumli <siad.ardroumli@googlemail.com>
2013-09-09 16:29:35 +0200

    Update Application.php

--- src/phpDocumentor/Application.php
+++ src/phpDocumentor/Application.php
@@ -276,6 +276,7 @@ class Application extends Cilex
         $config = $this['config']->toArray();
 
         $this['translator.locale'] = isset($config['translator']['locale']) ? $config['translator']['locale'] : 'en';
+        $app = $this;
         $this['translator'] = $this->share(
             function ($app) {
                 $translator = new Translator();
