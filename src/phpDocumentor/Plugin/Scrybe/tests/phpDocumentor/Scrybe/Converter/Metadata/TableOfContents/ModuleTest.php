c7e5d9b4ccd81fc77ccf305050063125f44e66e5
Mike van Riel <me@mikevanriel.com>
2014-01-26 21:17:50 +0100

    Change copyright year from 2013 to 2014

--- src/phpDocumentor/Plugin/Scrybe/tests/phpDocumentor/Scrybe/Converter/Metadata/TableOfContents/ModuleTest.php
+++ src/phpDocumentor/Plugin/Scrybe/tests/phpDocumentor/Scrybe/Converter/Metadata/TableOfContents/ModuleTest.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 5.3
  *
- * @copyright 2010-2013 Mike van Riel / Naenius (http://www.naenius.com)
+ * @copyright 2010-2014 Mike van Riel / Naenius (http://www.naenius.com)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://phpdoc.org
  */
