51c0ef23fbd7a2b54cdcbfba09317c3d158f302a
Mike van Riel <me@mikevanriel.com>
2014-01-26 21:09:23 +0100

    Add unit test for MarkerFromTagsExtracter compiler pass

--- src/phpDocumentor/Compiler/Pass/MarkerFromTagsExtractor.php
+++ src/phpDocumentor/Compiler/Pass/MarkerFromTagsExtractor.php
@@ -4,14 +4,16 @@
  *
  * PHP Version 5.3
  *
- * @copyright 2010-2013 Mike van Riel / Naenius (http://www.naenius.com)
+ * @copyright 2010-2014 Mike van Riel / Naenius (http://www.naenius.com)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://phpdoc.org
  */
 
 namespace phpDocumentor\Compiler\Pass;
 
+use phpDocumentor\Descriptor\Collection;
 use phpDocumentor\Descriptor\DescriptorAbstract;
+use phpDocumentor\Descriptor\FileDescriptor;
 use phpDocumentor\Descriptor\TagDescriptor;
 use phpDocumentor\Compiler\CompilerPassInterface;
 use phpDocumentor\Descriptor\ProjectDescriptor;
@@ -37,19 +39,52 @@ class MarkerFromTagsExtractor implements CompilerPassInterface
     public function execute(ProjectDescriptor $project)
     {
         /** @var DescriptorAbstract $element */
-        foreach ($project->getIndexes()->get('elements') as $element) {
+        foreach ($project->getIndexes()->get('elements', new Collection()) as $element) {
             $todos = $element->getTags()->get('todo', array());
 
             /** @var TagDescriptor $todo */
             foreach ($todos as $todo) {
-                $element->getFile()->getMarkers()->add(
-                    array(
-                        'type'    => 'TODO',
-                        'message' => $todo->getDescription(),
-                        'line'    => $element->getLine(),
-                    )
-                );
+                $fileDescriptor = $this->getFileDescriptor($element);
+                $this->addTodoMarker($fileDescriptor, $todo, $element);
             }
         }
+    }
+
+    /**
+     * Retrieves the File Descriptor from the given element.
+     *
+     * @param DescriptorAbstract $element
+     *
+     * @throws \UnexpectedValueException if the provided element does not have a file associated with it.
+     *
+     * @return FileDescriptor
+     */
+    protected function getFileDescriptor($element)
+    {
+        $fileDescriptor = $element instanceof FileDescriptor
+            ? $element
+            : $element->getFile();
+
+        if (!$fileDescriptor instanceof FileDescriptor) {
+            throw new \UnexpectedValueException('An element should always have a file associated with it');
+        }
+
+        return $fileDescriptor;
+    }
+
+    /**
+     * @param $fileDescriptor
+     * @param $todo
+     * @param $element
+     */
+    protected function addTodoMarker($fileDescriptor, $todo, $element)
+    {
+        $fileDescriptor->getMarkers()->add(
+            array(
+                'type' => 'TODO',
+                'message' => $todo->getDescription(),
+                'line' => $element->getLine(),
+            )
+        );
     }
 }
