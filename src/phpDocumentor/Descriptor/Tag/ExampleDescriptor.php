a9ff4ead9c38cb6e841db9a7444ffad79a838710
Siad Ardroumli <siad.ardroumli@googlemail.com>
2014-03-09 23:43:12 +0100

    Create ExampleDescriptor.php

---
+++ src/phpDocumentor/Descriptor/Tag/ExampleDescriptor.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.3
+ *
+ * @copyright 2010-2014 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Descriptor\Tag;
+
+use phpDocumentor\Descriptor\TagDescriptor;
+
+/**
+ * Descriptor representing the example tag with a descriptor.
+ */
+class ExampleDescriptor extends TagDescriptor
+{
+    /** @var string $content the content of the example. */
+    protected $content;
+
+    /** @var string $filePath the content of the example. */
+    protected $filePath;
+
+    /** @var int $lineCount the content of the example. */
+    protected $lineCount;
+
+    /** @var int $startingLine the content of the example. */
+    protected $startingLine;
+
+    /**
+     * Sets the location where the example points to.
+     *
+     * @param string $content
+     *
+     * @return void
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Returns the location where this example points to.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Sets the location where the example points to.
+     *
+     * @param string $filePath
+     *
+     * @return void
+     */
+    public function setFilePath($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    /**
+     * Returns the location where this example points to.
+     *
+     * @return string
+     */
+    public function getFilePath()
+    {
+        return $this->filePath;
+    }
+
+    /**
+     * Sets the starting line.
+     *
+     * @return void
+     */
+    public function setStartingLine($startingLine)
+    {
+        $this->startingLine = $startingLine;
+    }
+
+    /**
+     * Returns the starting line.
+     *
+     * @return int
+     */
+    public function getStartingLine()
+    {
+        return $this->startingLine;
+    }
+
+    /**
+     * Sets the line count.
+     *
+     * @return void
+     */
+    public function setLineCount($lineCount)
+    {
+        $this->lineCount = $lineCount;
+    }
+
+    /**
+     * Returns the line count.
+     *
+     * @return int
+     */
+    public function getLineCount()
+    {
+        return $this->lineCount;
+    }
+}
