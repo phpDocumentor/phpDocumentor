c3571830f7989eaa9b10175a10f5597bbc2008db
Mike van Riel <me@mikevanriel.com>
2014-03-29 12:41:10 +0100

    Fix bug in PR #1187
    After merging PR #1187 I discovered that it contained a bug in the order
    in which items were resolved. I have made this follow up commit to fix
    that.

--- src/phpDocumentor/Plugin/Twig/Extension.php
+++ src/phpDocumentor/Plugin/Twig/Extension.php
@@ -173,16 +173,14 @@ class Extension extends \Twig_Extension implements ExtensionInterface
                     $value = !is_array($value) ? array($value) : $value;
 
                     foreach ($value as $path) {
-                        $rule     = $routers->match($path);
-                        $generatedUrl = $rule->generate($path);
-
-                        if ($generatedUrl === false) {
-                            $url = false;
-                        } else {
-                            $url = $rule ? ltrim($generatedUrl, '/') : false;
+                        $url  = false;
+                        $rule = $routers->match($path);
+                        if ($rule) {
+                            $generatedUrl = $rule->generate($path);
+                            $url = $generatedUrl ? ltrim($generatedUrl, '/') : false;
                         }
 
-                        if ($url
+                        if (is_string($url)
                             && $url[0] != '/'
                             && (strpos($url, 'http://') !== 0)
                             && (strpos($url, 'https://') !== 0)
@@ -192,7 +190,6 @@ class Extension extends \Twig_Extension implements ExtensionInterface
 
                         switch ($presentation) {
                             case 'url': // return the first url
-
                                 return $url;
                             case 'class:short':
                                 $parts = explode('\\', $path);
