ffe88977d968ddf714c4e7e498f2edae050feff2
Mike van Riel <me@mikevanriel.com>
2014-05-16 11:28:40 +0200

    #1248: Update DocBlock for RunCommand

--- src/phpDocumentor/Command/Project/RunCommand.php
+++ src/phpDocumentor/Command/Project/RunCommand.php
@@ -4,11 +4,11 @@
  *
  * PHP Version 5.3
  *
- * @author    Mike van Riel <mike.vanriel@naenius.com>
- * @copyright 2010-2012 Mike van Riel / Naenius (http://www.naenius.com)
+ * @copyright 2010-2014 Mike van Riel / Naenius (http://www.naenius.com)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://phpdoc.org
  */
+
 namespace phpDocumentor\Command\Project;
 
 use phpDocumentor\Command\Command;
