16f2fdd746127524daa00e62ab68aba0a4b258bb
Siad Ardroumli <siad.ardroumli@googlemail.com>
2014-06-01 21:16:44 +0200

    Create PdfMerger.php

---
+++ src/phpDocumentor/Plugin/Pdf/Writer/PdfMerger.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.3
+ *
+ * @copyright 2010-2014 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Plugin\Pdf\Writer;
+
+use phpDocumentor\Descriptor\ProjectDescriptor;
+use phpDocumentor\Transformer\Transformation;
+use phpDocumentor\Transformer\Writer\WriterAbstract;
+use ZendPdf\PdfDocument;
+
+/**
+ * PdfMerger Writer.
+ *
+ * Usage in template.xml would be:
+ * `<transformation writer="PdfMerger" source="markers.pdf,errors.pdf,deprecated.pdf" artifact="docu.pdf"/>`
+ *
+ * @author Siad Ardroumli <siad.ardroumli@gmail.com>
+ */
+class PdfMerger extends WriterAbstract
+{
+    /**
+     * This method combines pdf documents at the given target target
+     * and creates a static pdf file at the artifact location.
+     *
+     * @param ProjectDescriptor $project        Document containing the structure.
+     * @param Transformation    $transformation Transformation to execute.
+     *
+     * @return void
+     */
+    public function transform(ProjectDescriptor $project, Transformation $transformation)
+    {
+        $source = $transformation->getSource();
+        $target = $transformation->getTransformer()->getTarget();
+
+        $pages = explode(',', $source);
+
+        $pdfMerged = new PdfDocument();
+
+        foreach ($pages as $page) {
+            $pdfDocument = PdfDocument::load($target . DIRECTORY_SEPARATOR . $page);
+
+            foreach ($pdfDocument->pages as $page) {
+                $clonedPage = clone $page;
+                $pdfMerged->pages[] = $clonedPage;
+            }
+        }
+
+        $pdfMerged->save(
+            $transformation->getTransformer()->getTarget()
+            . DIRECTORY_SEPARATOR
+            . $transformation->getArtifact()
+        );
+    }
+}
