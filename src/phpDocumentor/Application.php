424f8f171f0bee4cef5ecedb843ac24b9527f9cf
Siad Ardroumli <siad.ardroumli@googlemail.com>
2014-06-28 18:59:03 +0200

    Added version handling and added update-command

--- src/phpDocumentor/Application.php
+++ src/phpDocumentor/Application.php
@@ -57,7 +57,9 @@ class Application extends Cilex
     {
         $this->defineIniSettings();
         
-        self::$VERSION = trim(file_get_contents(__DIR__ . '/../../VERSION'));
+        self::$VERSION = strpos('@package_version@', '@') === 0
+            ? trim(file_get_contents(__DIR__ . '/../../VERSION'))
+            : '@package_version@';
 
         parent::__construct('phpDocumentor', self::$VERSION);
 
@@ -83,6 +85,7 @@ class Application extends Cilex
         $this->register(new Plugin\ServiceProvider());
 
         $this->addCommandsForProjectNamespace();
+        $this->addCommandsForPharNamespace();
     }
 
     /**
@@ -299,5 +302,15 @@ class Application extends Cilex
     protected function addCommandsForProjectNamespace()
     {
         $this->command(new Command\Project\RunCommand());
+    }
+
+    /**
+     * Adds the command to phpDocumentor that belong to the Phar namespace.
+     *
+     * @return void
+     */
+    protected function addCommandsForPharNamespace()
+    {
+        $this->command(new Command\Phar\UpdateCommand());
     }
 }
