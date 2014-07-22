abea571066b9633b4f7d905671b80301008823ca
Cees-Jan <ceesjank@gmail.com>
2014-07-22 20:59:19 +0200

    Removed ueless command

--- ansible/windows.sh
+++ ansible/windows.sh
@@ -6,5 +6,4 @@ sudo apt-get update
 sudo apt-get install -y ansible
 cp /vagrant/ansible/inventory /etc/ansible/hosts -f
 chmod 666 /etc/ansible/hosts
-cat /vagrant/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
 sudo ansible-playbook /vagrant/ansible/playbook.yml --connection=local
