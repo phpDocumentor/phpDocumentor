c4c4ae403b65d3c4cd93bf12ab62cf2e893921a2
Cees-Jan <ceesjank@gmail.com>
2014-07-22 21:12:08 +0200

    Removed ansible host copy

--- ansible/windows.sh
+++ ansible/windows.sh
@@ -4,6 +4,4 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:rquillo/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
-cp /vagrant/ansible/inventory /etc/ansible/hosts -f
-chmod 666 /etc/ansible/hosts
 sudo ansible-playbook /vagrant/ansible/playbook.yml --connection=local
