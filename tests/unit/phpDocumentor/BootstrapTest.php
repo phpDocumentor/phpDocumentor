d45e5cf525b534e097b6fdbb99545e0ab037f894
Jaap van Otterdijk <jaap@youngguns.nl>
2014-09-02 14:19:15 +0200

    tests for findVendorPath

--- tests/unit/phpDocumentor/BootstrapTest.php
+++ tests/unit/phpDocumentor/BootstrapTest.php
@@ -34,4 +34,56 @@ class BootstrapTest extends \PHPUnit_Framework_TestCase
         $bootstrap = Bootstrap::createInstance();
         $this->assertInstanceOf('phpDocumentor\Application', $bootstrap->initialize());
     }
+
+    /**
+     * @covers phpDocumentor\Bootstrap::findVendorPath();
+     */
+    public function testFindVendorPathStandAloneInstall()
+    {       
+        $structure = array(
+            'dummy' => array(
+                'vendor' => array(),
+                'src' => array(
+                    'phpDocumentor' => array(),
+                ),
+                'test' => array(),
+            ),
+        );
+
+        $root = \org\bovigo\vfs\vfsStream::setup('root', null, $structure);
+        $bootstrap = Bootstrap::createInstance();
+
+        $baseDir = \org\bovigo\vfs\vfsStream::url('root/dummy/src/phpDocumentor');
+        $this->assertSame('vfs://root/dummy/src/phpDocumentor/../../vendor' ,$bootstrap->findVendorPath($baseDir));
+    }
+
+    /**
+     * @covers phpDocumentor\Bootstrap::findVendorPath();
+     */
+    public function testFindVendorPathComposerInstalled()
+    {
+        $structure = array(
+            'dummy' => array(
+                'vendor' => array(
+                    'phpDocumentor' => array(
+                        'phpDocumentor' => array(
+                            'src' => array(
+                                'phpDocumentor' => array(),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        $root = \org\bovigo\vfs\vfsStream::setup('root', null, $structure);
+        \org\bovigo\vfs\vfsStream::newFile('composer.json')->at($root->getChild('dummy'));
+
+        $bootstrap = Bootstrap::createInstance();
+        $baseDir = \org\bovigo\vfs\vfsStream::url('root/dummy/vendor/phpDocumentor/phpDocumentor/src/phpDocumentor');
+        $this->assertSame(
+            'vfs://root/dummy/vendor/phpDocumentor/phpDocumentor/src/phpDocumentor/../../../../../vendor'
+            ,$bootstrap->findVendorPath($baseDir)
+        );
+    }
 }
