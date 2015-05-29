dd3fce7706f246d325a436681c103d41548878f6
Jaapio <jaap@ijaap.nl>
2015-05-29 23:16:29 +0200

    add api implementation

---
+++ src/phpDocumentor/ApiReference/Api.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.5
+ *
+ * @copyright 2010-2015 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\ApiReference;
+
+use phpDocumentor\DocumentGroupFormat;
+
+/**
+ * Container for Elements.
+ */
+final class Api
+{
+    /**
+     * Format of the api.
+     *
+     * @var DocumentGroupFormat
+     */
+    private $format;
+
+    /**
+     * Collection of elements in the api.
+     *
+     * @var Element[]
+     */
+    private $elements = [];
+
+    /**
+     * Initialized the class with the given format.
+     *
+     * @param DocumentGroupFormat $format
+     */
+    public function __construct(DocumentGroupFormat $format)
+    {
+        $this->format = $format;
+    }
+
+    /**
+     * Returns the format of this API.
+     *
+     * @return DocumentGroupFormat
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * Will return the Element when it is available. Otherwise returns null.
+     *
+     * @param Fqsen $fqsen
+     *
+     * @return Element|null
+     */
+    public function findElementByFqsen(Fqsen $fqsen)
+    {
+        if (isset($this->elements[(string)$fqsen])) {
+            return $this->elements[(string)$fqsen];
+        }
+
+        return null;
+    }
+
+    /**
+     * Add an element to the api.
+     *
+     * @param Element $element
+     *
+     * @return void
+     */
+    public function addElement(Element $element)
+    {
+        $this->elements[(string) $element->getFqsen()] = $element;
+    }
+
+    /**
+     * Returns all elements of the api.
+     *
+     * @return Element[]
+     */
+    public function getElements()
+    {
+        return $this->elements;
+    }
+}
---
+++ tests/unit/phpDocumentor/ApiReference/ApiTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.5
+ *
+ * @copyright 2010-2015 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\ApiReference;
+
+use Mockery as m;
+use phpDocumentor\DocumentGroupFormat;
+
+/**
+ * Test case for Api
+ * @coversDefaultClass phpDocumentor\ApiReference\Api
+ */
+class ApiTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::__construct
+     * @covers ::getFormat
+     */
+    public function testGetFormat()
+    {
+        $documentGroupFormat = new DocumentGroupFormat('api');
+        $api = new Api($documentGroupFormat);
+
+        $this->assertSame($documentGroupFormat, $api->getFormat());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::findElementByFqsen
+     */
+    public function testFindNotExistingElementByFqsen()
+    {
+        $documentGroupFormat = new DocumentGroupFormat('api');
+        $api = new Api($documentGroupFormat);
+
+        $this->assertNull($api->findElementByFqsen(new Fqsen('\My\Class')));
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::findElementByFqsen
+     * @covers ::addElement
+     */
+    public function testFindElementByFqsen()
+    {
+        $fqsen = new Fqsen('\My\Class');
+
+        $elementMock = m::mock(Element::class);
+        $elementMock
+            ->shouldReceive('getFqsen')
+            ->andReturn($fqsen);
+
+        $documentGroupFormat = new DocumentGroupFormat('api');
+        $api = new Api($documentGroupFormat);
+
+        $api->addElement($elementMock);
+
+        $this->assertSame($elementMock, $api->findElementByFqsen($fqsen));
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getElements
+     * @covers ::addElement
+     */
+    public function testGetElements()
+    {
+        $fqsen = new Fqsen('\My\Class');
+
+        $elementMock = m::mock(Element::class);
+        $elementMock
+            ->shouldReceive('getFqsen')
+            ->andReturn($fqsen);
+
+        $documentGroupFormat = new DocumentGroupFormat('api');
+        $api = new Api($documentGroupFormat);
+
+        $api->addElement($elementMock);
+
+        $this->assertEquals(
+            [
+                '\My\Class' => $elementMock,
+            ],
+            $api->getElements()
+        );
+    }
+}
