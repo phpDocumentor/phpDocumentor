ecfc4905bad5185ae43483bcb34611fb8c28e3cb
Marieke Bednarczyk <mbed@live.nl>
2015-06-05 15:57:18 +0200

    Added validation for Dsn components

--- src/phpDocumentor/Dsn.php
+++ src/phpDocumentor/Dsn.php
@@ -18,7 +18,6 @@ namespace phpDocumentor;
 
 final class Dsn
 {
-
     /** @var string */
     private $scheme;
 
@@ -42,6 +41,12 @@ final class Dsn
 
     /** @var array  */
     private $parameters = [];
+
+    /** @var array  */
+    private $locationParts = [];
+
+    /** @var array  */
+    private $dsnParts = [];
 
     /**
      * @param $dsn
@@ -120,36 +125,184 @@ final class Dsn
      */
     private function parse($dsn)
     {
-        $dsnParts = explode(';', $dsn);
-        $location = $dsnParts[0];
-        unset($dsnParts[0]);
-        $locationParts = parse_url($location);
+        $this->dsnParts = explode(';', $dsn);
+        $location = $this->dsnParts[0];
+        unset($this->dsnParts[0]);
+        $this->locationParts = parse_url($location);
 
-        $options = array();
-        foreach ($dsnParts as $part) {
-            $option = explode('=', $part);
-            $options[$option[0]] = $option[1];
+        if (! $this->locationParts) {
+            throw new \InvalidArgumentException(
+                sprintf('"%s" is not a valid DSN.', $dsn)
+            );
         }
 
-        $this->scheme = array_key_exists('scheme', $locationParts) ? $locationParts['scheme'] : 'file';
 
-        if (array_key_exists('scheme', $locationParts) && $locationParts['scheme'] === 'file') {
-            $path = array_key_exists('path', $locationParts) ? $locationParts['path'] : "";
-            $this->path = $locationParts['host'] . $path;
+        $this->parseScheme();
+
+        $this->parseHostAndPath();
+
+        $this->parsePort();
+
+        $this->parseUser();
+
+        $this->parsePassword();
+
+        $this->parseQuery();
+
+        $this->parseParameters();
+    }
+
+    /**
+     * validates and sets the scheme property
+     */
+    private function parseScheme()
+    {
+        if (array_key_exists('scheme', $this->locationParts)) {
+            $validSchemes = ['file', 'git+http', 'git+https'];
+            if (! in_array(strtolower($this->locationParts['scheme']), $validSchemes)) {
+                throw new \InvalidArgumentException(
+                    sprintf('"%s" is not a valid scheme.', $this->locationParts['scheme'])
+                );
+            }
+            $this->scheme = $this->locationParts['scheme'];
+
         } else {
-            $this->host = array_key_exists('host', $locationParts) ? $locationParts['host'] : "";
-            $this->path = array_key_exists('path', $locationParts) ? $locationParts['path'] : "";
+
+            $this->scheme = 'file';
+        }
+    }
+
+    /**
+     * Validates and sets the host and path properties
+     */
+    private function parseHostAndPath()
+    {
+        if (array_key_exists('scheme', $this->locationParts) && $this->locationParts['scheme'] === 'file') {
+            $path = array_key_exists('path', $this->locationParts) ? $this->locationParts['path'] : "";
+            $path = $this->locationParts['host'] . $path;
+            $result = preg_match("/^([a-zA-Z0-9-_.~\/]*(%([A-F]|[0-9]){2})*)*$/", $path);
+            if ($result === 0) {
+                throw new \InvalidArgumentException(
+                    sprintf('"%s" is not a valid path.', $this->locationParts['path'])
+                );
+            }
+
+            $this->path = $path;
+
+        } else {
+            if (array_key_exists('host', $this->locationParts)) {
+                $result = preg_match("/^([a-zA-Z0-9-_.~]*(%([A-F]|[0-9]){2})*)*$/", $this->locationParts['host']);
+                if ($result === 0) {
+                    throw new \InvalidArgumentException(
+                        sprintf('"%s" is not a valid host.', $this->locationParts['host'])
+                    );
+                }
+                $this->host = $this->locationParts['host'];
+            }
+
+            if (array_key_exists('path', $this->locationParts)) {
+                $result = preg_match("/^([a-zA-Z0-9-_.~\/]*(%([A-F]|[0-9]){2})*)*$/", $this->locationParts['path']);
+                if ($result === 0) {
+                    throw new \InvalidArgumentException(
+                        sprintf('"%s" is not a valid path.', $this->locationParts['path'])
+                    );
+                }
+                $this->path = $this->locationParts['path'];
+            }
+        }
+    }
+
+    /**
+     * validates and sets the port property
+     */
+    private function parsePort()
+    {
+        if (array_key_exists('port', $this->locationParts)) {
+            if (!is_int($this->locationParts['port'])) {
+                throw new \InvalidArgumentException(
+                    sprintf('"%s" is not a valid port.', $this->locationParts['port'])
+                );
+            }
+            $this->port = $this->locationParts['port'];
+        }
+    }
+
+    /**
+     * validates and sets the user property
+     */
+    private function parseUser()
+    {
+        if (array_key_exists('user', $this->locationParts)) {
+            $result = preg_match("/^([a-zA-Z0-9-_.~!$&'()+*,=]*(%([A-F]|[0-9]){2})*)*$/", $this->locationParts['user']);
+
+            if ($result === 0) {
+                throw new \InvalidArgumentException(
+                    sprintf('"%s" is not a valid username.', $this->locationParts['user'])
+                );
+            }
+            $this->user = $this->locationParts['user'];
         }
 
-        $this->port = array_key_exists('port', $locationParts) ? $locationParts['port'] : "";
+    }
+    /**
+     * validates and sets the password property
+     */
+    private function parsePassword()
+    {
+        if (array_key_exists('pass', $this->locationParts)) {
+            $result = preg_match(
+                "/^([a-zA-Z0-9-_.~!$&'()+*,=:]*(%([A-F]|[0-9]){2})*)*$/",
+                $this->locationParts['pass']
+            );
 
-        $this->user = array_key_exists('user', $locationParts) ? $locationParts['user'] : "";
+            if ($result === 0) {
+                throw new \InvalidArgumentException(
+                    sprintf('"%s" is not a valid password.', $this->locationParts['pass'])
+                );
+            }
+            $this->password = $this->locationParts['pass'];
+        }
 
-        $this->password = array_key_exists('pass', $locationParts) ? $locationParts['pass'] : "";
+    }
 
-        $query = array_key_exists('query', $locationParts) ? $locationParts['query'] : "";
-        $this->query = !empty($query) ? explode('?', $query) : [];
+    /**
+     * validates and sets the query property
+     */
+    private function parseQuery()
+    {
+        if (array_key_exists('query', $this->locationParts)) {
+            $queryParts = explode('?', $this->locationParts['query']);
+            foreach ($queryParts as $part) {
+                $result = preg_match("/^([a-zA-Z0-9-_.~!$&'()+*,=:\/@]*(%([A-F]|[0-9]){2})*)*$/", $part);
 
-        $this->parameters = !empty($options) ? $options : [];
+                if ($result === 0) {
+                    throw new \InvalidArgumentException(
+                        sprintf('"%s" is not a valid query.', $part)
+                    );
+                }
+            }
+            $this->query = $queryParts;
+        }
+    }
+
+    /**
+     * validates and sets the parameters property
+     */
+    private function parseParameters()
+    {
+        if (!empty($this->dsnParts)) {
+            foreach ($this->dsnParts as $part) {
+                $result = preg_match("/^[a-zA-Z0-9-_]+=([a-zA-Z0-9-_.~!$&'()+*,=:\/@]*(%([A-F]|[0-9]){2})*)*$/", $part);
+
+                if ($result === 0) {
+                    throw new \InvalidArgumentException(
+                        sprintf('"%s" is not a valid parameter.', $part)
+                    );
+                }
+
+                $option = explode('=', $part);
+                $this->parameters[$option[0]] = $option[1];
+            }
+        }
     }
 }
