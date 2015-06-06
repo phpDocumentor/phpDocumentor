a13510d6586999f44951536ab93386cc73d018df
Marieke Bednarczyk <mbed@live.nl>
2015-06-06 16:49:38 +0200

    Cleaned up Dsn.php and applied advice

--- src/phpDocumentor/Dsn.php
+++ src/phpDocumentor/Dsn.php
@@ -15,7 +15,6 @@ namespace phpDocumentor;
 /**
  * Value Object for DSN.
  */
-
 final class Dsn
 {
     /** @var string */
@@ -36,20 +35,16 @@ final class Dsn
     /** @var string */
     private $path;
 
-    /** @var array  */
+    /** @var string[]  */
     private $query = [];
 
-    /** @var array  */
+    /** @var string[]  */
     private $parameters = [];
 
-    /** @var array  */
-    private $locationParts = [];
-
-    /** @var array  */
-    private $dsnParts = [];
-
     /**
-     * @param $dsn
+     * Initializes the Dsn
+     *
+     * @param string $dsn
      */
     public function __construct($dsn)
     {
@@ -57,6 +52,8 @@ final class Dsn
     }
 
     /**
+     * Returns the scheme part of the DSN
+     *
      * @return string
      */
     public function getScheme()
@@ -65,6 +62,8 @@ final class Dsn
     }
 
     /**
+     * Returns the host part of the DSN
+     *
      * @return string
      */
     public function getHost()
@@ -73,6 +72,8 @@ final class Dsn
     }
 
     /**
+     * Returns the port part of the DSN
+     *
      * @return int
      */
     public function getPort()
@@ -81,6 +82,8 @@ final class Dsn
     }
 
     /**
+     * Returns the username part of the DSN
+     *
      * @return string
      */
     public function getUsername()
@@ -89,6 +92,8 @@ final class Dsn
     }
 
     /**
+     * Returns the password part of the DSN
+     *
      * @return string
      */
     public function getPassword()
@@ -97,6 +102,8 @@ final class Dsn
     }
 
     /**
+     * Returns the path part of the DSN
+     *
      * @return string
      */
     public function getPath()
@@ -105,7 +112,9 @@ final class Dsn
     }
 
     /**
-     * @return string
+     * Returns the query part of the DSN
+     *
+     * @return string[]
      */
     public function getQuery()
     {
@@ -113,7 +122,9 @@ final class Dsn
     }
 
     /**
-     * @return string
+     * Returns the parameters part of the DSN
+     *
+     * @return string[]
      */
     public function getParameters()
     {
@@ -121,180 +132,93 @@ final class Dsn
     }
 
     /**
-     * @param $dsn
+     * Parses the given DSN
+     *
+     * @param string $dsn
      */
     private function parse($dsn)
     {
-        $this->dsnParts = explode(';', $dsn);
-        $location = $this->dsnParts[0];
-        unset($this->dsnParts[0]);
-        $this->locationParts = parse_url($location);
-
-        if (! $this->locationParts) {
+        if (! is_string($dsn)) {
             throw new \InvalidArgumentException(
                 sprintf('"%s" is not a valid DSN.', $dsn)
             );
         }
 
+        $dsnParts = explode(';', $dsn);
+        $location = $dsnParts[0];
+        unset($dsnParts[0]);
+        $locationParts = parse_url($location);
 
-        $this->parseScheme();
+        if (! isset($locationParts['scheme'])) {
+            $locationParts['scheme'] = 'file';
+            $location = 'file://' . $location;
+        }
 
-        $this->parseHostAndPath();
+        if (! filter_var($location, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException(
+                sprintf('"%s" is not a valid DSN.', $dsn)
+            );
+        }
 
-        $this->parsePort();
+        $this->parseScheme($locationParts);
 
-        $this->parseUser();
+        $this->parseHostAndPath($locationParts);
 
-        $this->parsePassword();
+        $this->port = isset($locationParts['port']) ? $locationParts['port'] : "";
 
-        $this->parseQuery();
+        $this->user = isset($locationParts['user']) ? $locationParts['user'] : "";
 
-        $this->parseParameters();
+        $this->password = isset($locationParts['pass']) ? $locationParts['pass'] : "";
+
+        $this->query = isset($locationParts['query']) ? explode('?', $locationParts['query']) : "";
+
+        $this->parseParameters($dsnParts);
     }
 
     /**
      * validates and sets the scheme property
+     *
+     * @return void
      */
-    private function parseScheme()
+    private function parseScheme($locationParts)
     {
-        if (array_key_exists('scheme', $this->locationParts)) {
-            $validSchemes = ['file', 'git+http', 'git+https'];
-            if (! in_array(strtolower($this->locationParts['scheme']), $validSchemes)) {
-                throw new \InvalidArgumentException(
-                    sprintf('"%s" is not a valid scheme.', $this->locationParts['scheme'])
-                );
-            }
-            $this->scheme = $this->locationParts['scheme'];
-
-        } else {
-
-            $this->scheme = 'file';
+        $validSchemes = ['file', 'git+http', 'git+https'];
+        if (! in_array(strtolower($locationParts['scheme']), $validSchemes)) {
+            throw new \InvalidArgumentException(
+                sprintf('"%s" is not a valid scheme.', $locationParts['scheme'])
+            );
         }
+        $this->scheme = strtolower($locationParts['scheme']);
     }
 
     /**
      * Validates and sets the host and path properties
+     *
+     * @return void
      */
-    private function parseHostAndPath()
+    private function parseHostAndPath($locationParts)
     {
-        if (array_key_exists('scheme', $this->locationParts) && $this->locationParts['scheme'] === 'file') {
-            $path = array_key_exists('path', $this->locationParts) ? $this->locationParts['path'] : "";
-            $path = $this->locationParts['host'] . $path;
-            $result = preg_match("/^([a-zA-Z0-9-_.~\/]*(%([A-F]|[0-9]){2})*)*$/", $path);
-            if ($result === 0) {
-                throw new \InvalidArgumentException(
-                    sprintf('"%s" is not a valid path.', $this->locationParts['path'])
-                );
-            }
+        $path = isset($locationParts['path']) ? $locationParts['path'] : "";
+        $host = isset($locationParts['host']) ? $locationParts['host'] : "";
 
-            $this->path = $path;
-
+        if ($this->getScheme() === 'file') {
+            $this->path = $host . $path;
         } else {
-            if (array_key_exists('host', $this->locationParts)) {
-                $result = preg_match("/^([a-zA-Z0-9-_.~]*(%([A-F]|[0-9]){2})*)*$/", $this->locationParts['host']);
-                if ($result === 0) {
-                    throw new \InvalidArgumentException(
-                        sprintf('"%s" is not a valid host.', $this->locationParts['host'])
-                    );
-                }
-                $this->host = $this->locationParts['host'];
-            }
-
-            if (array_key_exists('path', $this->locationParts)) {
-                $result = preg_match("/^([a-zA-Z0-9-_.~\/]*(%([A-F]|[0-9]){2})*)*$/", $this->locationParts['path']);
-                if ($result === 0) {
-                    throw new \InvalidArgumentException(
-                        sprintf('"%s" is not a valid path.', $this->locationParts['path'])
-                    );
-                }
-                $this->path = $this->locationParts['path'];
-            }
-        }
-    }
-
-    /**
-     * validates and sets the port property
-     */
-    private function parsePort()
-    {
-        if (array_key_exists('port', $this->locationParts)) {
-            if (!is_int($this->locationParts['port'])) {
-                throw new \InvalidArgumentException(
-                    sprintf('"%s" is not a valid port.', $this->locationParts['port'])
-                );
-            }
-            $this->port = $this->locationParts['port'];
-        }
-    }
-
-    /**
-     * validates and sets the user property
-     */
-    private function parseUser()
-    {
-        if (array_key_exists('user', $this->locationParts)) {
-            $result = preg_match("/^([a-zA-Z0-9-_.~!$&'()+*,=]*(%([A-F]|[0-9]){2})*)*$/", $this->locationParts['user']);
-
-            if ($result === 0) {
-                throw new \InvalidArgumentException(
-                    sprintf('"%s" is not a valid username.', $this->locationParts['user'])
-                );
-            }
-            $this->user = $this->locationParts['user'];
-        }
-
-    }
-    /**
-     * validates and sets the password property
-     */
-    private function parsePassword()
-    {
-        if (array_key_exists('pass', $this->locationParts)) {
-            $result = preg_match(
-                "/^([a-zA-Z0-9-_.~!$&'()+*,=:]*(%([A-F]|[0-9]){2})*)*$/",
-                $this->locationParts['pass']
-            );
-
-            if ($result === 0) {
-                throw new \InvalidArgumentException(
-                    sprintf('"%s" is not a valid password.', $this->locationParts['pass'])
-                );
-            }
-            $this->password = $this->locationParts['pass'];
-        }
-
-    }
-
-    /**
-     * validates and sets the query property
-     */
-    private function parseQuery()
-    {
-        if (array_key_exists('query', $this->locationParts)) {
-            $queryParts = explode('?', $this->locationParts['query']);
-            foreach ($queryParts as $part) {
-                $result = preg_match("/^([a-zA-Z0-9-_.~!$&'()+*,=:\/@]*(%([A-F]|[0-9]){2})*)*$/", $part);
-
-                if ($result === 0) {
-                    throw new \InvalidArgumentException(
-                        sprintf('"%s" is not a valid query.', $part)
-                    );
-                }
-            }
-            $this->query = $queryParts;
+            $this->host = $host;
+            $this->path = $path;
         }
     }
 
     /**
      * validates and sets the parameters property
+     *
+     * @return void
      */
-    private function parseParameters()
+    private function parseParameters($dsnParts)
     {
-        if (!empty($this->dsnParts)) {
-            foreach ($this->dsnParts as $part) {
-                $result = preg_match("/^[a-zA-Z0-9-_]+=([a-zA-Z0-9-_.~!$&'()+*,=:\/@]*(%([A-F]|[0-9]){2})*)*$/", $part);
-
-                if ($result === 0) {
+        if (!empty($dsnParts)) {
+            foreach ($dsnParts as $part) {
+                if (! strpos($part, '=')) {
                     throw new \InvalidArgumentException(
                         sprintf('"%s" is not a valid parameter.', $part)
                     );
