2ce498f2d7ef51231c9c48a63357f7813f7a502a
Jaapio <jaap@ijaap.nl>
2015-08-09 21:41:17 +0200

    add flysystem adapter for file strategy

---
+++ src/phpDocumentor/Reflection/Php/Factory/File/FlySystemAdapter.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2010-2015 Mike van Riel<mike@phpdoc.org>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Reflection\Php\Factory\File;
+
+
+use League\Flysystem\Filesystem;
+
+final class FlySystemAdapter implements Adapter
+{
+    /**
+     * Filesystem used to read file.
+     *
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * Returns true when the file exists.
+     *
+     * @param string $filePath
+     * @return boolean
+     */
+    public function fileExists($filePath)
+    {
+        return $this->filesystem->has($filePath);
+    }
+
+    /**
+     * Returns the content of the file as a string.
+     *
+     * @param $filePath
+     * @return string
+     */
+    public function getContents($filePath)
+    {
+        return $this->filesystem->read($filePath);
+    }
+
+    /**
+     * Returns md5 hash of the file.
+     *
+     * @param $filePath
+     * @return string
+     */
+    public function md5($filePath)
+    {
+        return md5($this->getContents());
+    }
+
+    /**
+     * Returns an relative path to the file.
+     *
+     * @param string $filePath
+     * @return string
+     */
+    public function path($filePath)
+    {
+        return $filePath;
+    }
+}
