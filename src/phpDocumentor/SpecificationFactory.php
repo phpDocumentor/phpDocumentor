3ec22372da75156c2668f28bba2a2e4377373b4c
Jaapio <jaap@ijaap.nl>
2015-08-16 15:15:15 +0200

    added source paths to spec

--- src/phpDocumentor/SpecificationFactory.php
+++ src/phpDocumentor/SpecificationFactory.php
@@ -22,10 +22,10 @@ interface SpecificationFactory
     /**
      * Creates a SpecificationInterface object based on the ignore and extension parameters.
      *
+     * @param array $paths
      * @param array $ignore
      * @param array $extensions
-     *
      * @return SpecificationInterface
      */
-    public function create(array $ignore, array $extensions);
+    public function create(array $paths, array $ignore, array $extensions);
 }
