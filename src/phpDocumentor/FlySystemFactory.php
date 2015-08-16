7934ecbcf2e8630f6aa11caf4d9995325fe0cdaf
Jaapio <jaap@ijaap.nl>
2015-08-16 15:50:15 +0200

    register FlyFinder to filesystems

--- src/phpDocumentor/FlySystemFactory.php
+++ src/phpDocumentor/FlySystemFactory.php
@@ -12,6 +12,7 @@
 
 namespace phpDocumentor;
 
+use Flyfinder\Finder;
 use \LogicException;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
@@ -49,7 +50,7 @@ final class FlySystemFactory implements FileSystemFactory
             if ($dsn->getScheme() === 'file') {
 
                 $path = $dsn->getPath();
-                $filesystem = new Filesystem(new Local($path));
+                $filesystem = new Filesystem(new Local($path, LOCK_EX, Local::SKIP_LINKS));
 
             } else {
                 //This will be implemented as soon as the CloneRemoteGitToLocal adapter is finished
@@ -59,6 +60,7 @@ final class FlySystemFactory implements FileSystemFactory
             $this->mountManager->mountFilesystem($dsnId, $filesystem);
         }
 
+        $filesystem->addPlugin(new Finder());
         return $filesystem;
     }
 }
