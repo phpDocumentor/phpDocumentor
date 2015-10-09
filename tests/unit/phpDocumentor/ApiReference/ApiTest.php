435b6c2882863824a428333d50c7710e317e46b1
Mike van Riel <me@mikevanriel.com>
2015-10-09 17:25:56 +0200

    Restored the test for API handling

--- tests/unit/phpDocumentor/ApiReference/ApiTest.php
+++ tests/unit/phpDocumentor/ApiReference/ApiTest.php
@@ -15,87 +15,86 @@ use Mockery as m;
 use phpDocumentor\DocumentGroupFormat;
 use phpDocumentor\Reflection\Element;
 use phpDocumentor\Reflection\Fqsen;
-use phpDocumentor\Reflection\Php\Class_;
-use phpDocumentor\Reflection\Php\File;
 use phpDocumentor\Reflection\Php\Project;
 
 /**
  * Test case for Api
  * @coversDefaultClass phpDocumentor\ApiReference\Api
+ * @covers ::__construct
  */
 class ApiTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var Api */
+    private $api;
+
+    /** @var DocumentGroupFormat */
+    private $documentGroupFormat;
+
+    /** @var Project */
+    private $project;
+
     /**
-     * @covers ::__construct
+     * Initializes this Reference object for API elements.
+     */
+    public function setUp()
+    {
+        $this->documentGroupFormat = new DocumentGroupFormat('api');
+        $this->project             = new Project('MyProject');
+        $this->api                 = new Api($this->documentGroupFormat, $this->project);
+    }
+
+    /**
      * @covers ::getFormat
      */
     public function testGetFormat()
     {
-        $documentGroupFormat = new DocumentGroupFormat('api');
-        $api = new Api($documentGroupFormat, new Project('MyProject'));
-
-        $this->assertSame($documentGroupFormat, $api->getFormat());
+        $this->assertSame($this->documentGroupFormat, $this->api->getFormat());
     }
 
     /**
-     * @covers ::__construct
-     * @covers ::findElementByFqsen
-     */
-    public function testFindNotExistingElementByFqsen()
-    {
-        $documentGroupFormat = new DocumentGroupFormat('api');
-        $api = new Api($documentGroupFormat, new Project('MyProject'));
-
-        $this->assertNull($api->findElementByFqsen(new Fqsen('\My\Class')));
-    }
-
-    /**
-     * @covers ::__construct
-     * @covers ::findElementByFqsen
-     */
-    public function testFindElementByFqsen()
-    {
-        $this->markTestIncomplete('needs rework');
-        $fqsen = new Fqsen('\My\Class');
-
-        $elementMock = m::mock(Element::class);
-        $elementMock
-            ->shouldReceive('getFqsen')
-            ->andReturn($fqsen);
-
-        $documentGroupFormat = new DocumentGroupFormat('api');
-        $api = new Api($documentGroupFormat, $project);
-
-        $api->addElement($elementMock);
-
-        $this->assertSame($elementMock, $api->findElementByFqsen($fqsen));
-    }
-
-    /**
-     * @covers ::__construct
-     * @covers ::getElements
      * @covers ::addElement
+     * @covers ::findElementByFqsen
      */
-    public function testGetElements()
+    public function testAddingAndRetrievingAnElement()
     {
-        $this->markTestIncomplete('needs rework');
-        $fqsen = new Fqsen('\My\Class');
+        $fqsen   = new Fqsen('\\My\\Space');
+        $element = $this->givenAnElementWithFqsen($fqsen);
 
-        $elementMock = m::mock(Element::class);
-        $elementMock
-            ->shouldReceive('getFqsen')
-            ->andReturn($fqsen);
+        $this->api->addElement($element);
+        $this->assertSame($element, $this->api->findElementByFqsen($fqsen));
+    }
 
-        $documentGroupFormat = new DocumentGroupFormat('api');
-        $api = new Api($documentGroupFormat);
+    /**
+     * @covers ::findElementByFqsen
+     */
+    public function testNullIsReturnedWhenFindingAnUnknownElement()
+    {
+        $this->assertNull($this->api->findElementByFqsen(new Fqsen('\My\Class')));
+    }
 
-        $api->addElement($elementMock);
+    /**
+     * @covers ::getElements
+     */
+    public function testRetrievingAllElementsReturnsAListOfElements()
+    {
+        $fqsenString = '\\My\\Space';
+        $element = $this->givenAnElementWithFqsen(new Fqsen($fqsenString));
 
-        $this->assertEquals(
-            [
-                '\My\Class' => $elementMock,
-            ],
-            $api->getElements()
-        );
+        $this->api->addElement($element);
+        $this->assertSame([$fqsenString => $element], $this->api->getElements());
+    }
+
+    /**
+     * Returns a mocked Element that will return the given FQSEN when asked for.
+     *
+     * @param Fqsen $fqsen
+     *
+     * @return m\MockInterface|Element
+     */
+    private function givenAnElementWithFqsen($fqsen)
+    {
+        return m::mock(Element::class)
+            ->shouldReceive('getFqsen')->andReturn($fqsen)
+            ->getMock();
     }
 }
