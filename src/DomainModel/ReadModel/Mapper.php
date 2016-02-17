c88da3ba51fb0614c0aefd63e1090eced7c59f47
Mike van Riel <me@mikevanriel.com>
2016-02-17 07:31:28 +0100

    Added copyright docblocks and finals to ReadModel

--- src/DomainModel/ReadModel/Mapper.php
+++ src/DomainModel/ReadModel/Mapper.php
@@ -1,6 +1,18 @@
 <?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2010-2016 Mike van Riel<mike@phpdoc.org>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
 
 namespace phpDocumentor\DomainModel\ReadModel;
+
+use phpDocumentor\DomainModel\Parser\Documentation;
 
 interface Mapper
 {
@@ -8,9 +20,9 @@ interface Mapper
      * Returns the data needed by the ViewFactory to create a new View.
      *
      * @param Definition $readModelDefinition
-     * @param                $documentation
+     * @param Documentation $documentation
      *
      * @return mixed
      */
-    public function create(Definition $readModelDefinition, $documentation);
+    public function create(Definition $readModelDefinition, Documentation $documentation);
 }
