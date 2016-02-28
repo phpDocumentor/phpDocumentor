634540eebfdafbcec0ec5992becb7c4a5f475f14
Mike van Riel <me@mikevanriel.com>
2016-02-28 21:29:29 +0100

    Add test for HTML Documents

--- tests/unit/DomainModel/Parser/Version/DefinitionTest.php
+++ tests/unit/DomainModel/Parser/Version/DefinitionTest.php
@@ -21,15 +21,26 @@ use phpDocumentor\DomainModel\Parser\Documentation\DummyDocumentGroupDefinition;
 class DefinitionTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @test
      * @covers ::getVersionNumber
      * @covers ::getDocumentGroupDefinitions
      */
-    public function testVersionNumber()
+    public function itShouldReceiveAVersionNumberAndListOfDocumentGroupDefinitions()
     {
         $documentGroupDefinitions = [new DummyDocumentGroupDefinition()];
         $definition = new Definition(new Number('1.0.1'), $documentGroupDefinitions);
 
         $this->assertEquals(new Number('1.0.1'), $definition->getVersionNumber());
         $this->assertSame($documentGroupDefinitions, $definition->getDocumentGroupDefinitions());
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     */
+    public function itShouldErrorIfTheDocumentGroupDefinitionsArrayContainsSomethingOtherThanADefinition()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Definition(new Number('1.0.1'), ['abc']);
     }
 }
