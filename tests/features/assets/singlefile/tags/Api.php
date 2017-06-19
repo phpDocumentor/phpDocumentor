d250e25337d97b61447377dbeeef5d432f43d801
Jaapio <jaap@ijaap.nl>
2017-06-19 16:10:04 +0200

    Adds tests for api tag

---
+++ tests/features/assets/singlefile/tags/Api.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Class A
+ *
+ * @api
+ */
+class A
+{
+
+    /**
+     * @api
+     */
+    public function someMethod()
+    {
+
+    }
+
+    public function someOtherMethod()
+    {
+
+    }
+}
+
+class B extends A
+{
+}
