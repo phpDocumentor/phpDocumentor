982ee15433f1b07e5ddac0652040eb0510ff606d
Jaapio <jaap@ijaap.nl>
2017-06-19 16:10:04 +0200

    Adds tests for api tag

--- tests/features/bootstrap/Ast/ApiContext.php
+++ tests/features/bootstrap/Ast/ApiContext.php
@@ -17,8 +17,12 @@ use Behat\Gherkin\Node\PyStringNode;
 use org\bovigo\vfs\vfsStream;
 use phpDocumentor\Behat\Contexts\EnvironmentContext;
 use phpDocumentor\Descriptor\ClassDescriptor;
+use phpDocumentor\Descriptor\Collection;
+use phpDocumentor\Descriptor\DescriptorAbstract;
 use phpDocumentor\Descriptor\FileDescriptor;
 use phpDocumentor\Descriptor\ProjectDescriptor;
+use phpDocumentor\Descriptor\Tag\VersionDescriptor;
+use phpDocumentor\Reflection\DocBlock\Tag;
 use PHPUnit\Framework\Assert;
 
 class ApiContext implements Context
@@ -31,7 +35,7 @@ class ApiContext implements Context
     {
         $environment = $scope->getEnvironment();
 
-        $this->minkContext = $environment->getContext(EnvironmentContext::class);
+        $this->minkContext = $environment->getContext('phpDocumentor\Behat\Contexts\EnvironmentContext');
     }
 
     /**
@@ -59,7 +63,7 @@ class ApiContext implements Context
      */
     public function theASTHasAClassInDefaultPackage($class)
     {
-        $class = $this->findClass($class);
+        $class = $this->findClassByName($class);
 
         Assert::assertEquals('Default', $class->getPackage()->getName());
     }
@@ -71,9 +75,92 @@ class ApiContext implements Context
      */
     public function classHasDocblockWithContent($class, PyStringNode $expectedContent)
     {
-        $class = $this->findClass($class);
+        $class = $this->findClassByName($class);
 
         Assert::assertEquals($expectedContent->getRaw(), $class->getDescription());
+    }
+
+    /**
+     * @param $classFqsen
+     * @param $docElement
+     * @param $value
+     * @Then class ":classFqsen" has :docElement:
+     */
+    public function classHasDocblockContent($classFqsen, $docElement, PyStringNode $value)
+    {
+        $class = $this->findClassByFqsen($classFqsen);
+
+        $method = 'get' . $docElement;
+
+        Assert::assertEquals($value->getRaw(), $class->$method());
+    }
+
+    /**
+     * @param $classFqsen
+     * @param $docElement
+     * @param $value
+     * @Then class ":classFqsen" has version :value
+     */
+    public function classHasVersion($classFqsen, $value)
+    {
+        $class = $this->findClassByFqsen($classFqsen);
+
+        /** @var VersionDescriptor $tag */
+        foreach ($class->getVersion() as $tag) {
+            if($tag->getVersion() === $value) {
+                return;
+            }
+        }
+
+        Assert::fail(sprintf('Didn\'t find expected version "%s"', $value));
+    }
+
+    /**
+     * @param $classFqsen
+     * @param $tag
+     * @Then class ":classFqsen" without tag :tagName
+     */
+    public function classWithoutTag($classFqsen, $tagName)
+    {
+        $this->classHasTag($classFqsen, $tagName, 0);
+    }
+
+    /**
+     * @param $classFqsen
+     * @param $tag
+     * @Then class ":classFqsen" has exactly :expectedNumber tag :tagName
+     */
+    public function classHasTag($classFqsen, $tagName, $expectedNumber)
+    {
+        $class = $this->findClassByFqsen($classFqsen);
+        static::AssertTagCount($class, $tagName, $expectedNumber);
+    }
+
+    /**
+     * @param string $classFqsen
+     * @param string $tagName
+     * @param string $method
+     * @internal param $tag
+     * @Then class ":classFqsen" has a method named :method without tag :tagName
+     */
+    public function classHasMethodWithoutTag($classFqsen, $tagName, $method)
+    {
+        $this->classHasMethodWithExpectedCountTag($classFqsen, $tagName, $method, 0);
+    }
+
+    /**
+     * @param string $classFqsen
+     * @param string $tagName
+     * @param string $methodName
+     * @internal param $tag
+     * @Then class ":classFqsen" has a method named :method with exactly :expected tag :tagName
+     */
+    public function classHasMethodWithExpectedCountTag($classFqsen, $tagName, $methodName, $expectedCount)
+    {
+        $class = $this->findClassByFqsen($classFqsen);
+        $method = $class->getMethods()->get($methodName);
+
+        static::AssertTagCount($method, $tagName, $expectedCount);
     }
 
     /**
@@ -81,7 +168,7 @@ class ApiContext implements Context
      * @return ClassDescriptor
      * @throws \Exception
      */
-    private function findClass($className) {
+    private function findClassByName($className) {
         $ast = $this->getAst();
         foreach ($ast->getFiles() as $file) {
             foreach ($file->getClasses() as $classDescriptor) {
@@ -92,6 +179,26 @@ class ApiContext implements Context
         }
 
         throw new \Exception(sprintf('Didn\'t find expected class "%s"', $className));
+    }
+
+    /**
+     * @param string $classFqsen
+     * @return ClassDescriptor
+     * @throws \Exception
+     */
+    private function findClassByFqsen($classFqsen)
+    {
+        $ast = $this->getAst();
+        foreach ($ast->getFiles() as $file) {
+            /** @var ClassDescriptor $classDescriptor */
+            foreach ($file->getClasses() as $classDescriptor) {
+                if ($classDescriptor->getFullyQualifiedStructuralElementName() === $classFqsen) {
+                    return $classDescriptor;
+                }
+            }
+        }
+
+        throw new \Exception(sprintf('Didn\'t find expected class "%s"', $classFqsen));
     }
 
     /**
@@ -108,5 +215,21 @@ class ApiContext implements Context
         }
 
         return unserialize(file_get_contents($file));
+    }
+
+    /**
+     * @param string $tagName
+     * @param int $expectedNumber
+     * @param DescriptorAbstract $element
+     */
+    private static function AssertTagCount($element, $tagName, $expectedNumber)
+    {
+        /** @var Collection $tagCollection */
+        $tagCollection = $element->getTags()->get($tagName, new Collection());
+
+        Assert::assertEquals((int)$expectedNumber, $tagCollection->count());
+        if ($expectedNumber > 0) {
+            Assert::assertEquals($tagName, $tagCollection[0]->getName());
+        }
     }
 }
