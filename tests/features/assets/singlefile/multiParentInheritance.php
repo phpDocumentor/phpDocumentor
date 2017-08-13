1dd8c18959147e9629de737784f14bc6fd050655
Jaapio <jaap@ijaap.nl>
2017-08-13 20:54:56 +0200

    Adds behat test for method summary inheritance
    This adds a testcase for the expected behavior of inline inheritance of
    method summary.
    
    refs #1811

---
+++ tests/features/assets/singlefile/multiParentInheritance.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ *  @copyright 2010-2017 Mike van Riel<mike@phpdoc.org>
+ *  @license   http://www.opensource.org/licenses/mit-license.php MIT
+ *  @link      http://phpdoc.org
+ */
+
+interface ExampleInterface
+{
+    /**
+     * Do something with $object and return that it worked
+     *
+     * @param \stdClass $object The object
+     * @return bool
+     * @internal
+     */
+    public function doSomething(\stdClass $object);
+}
+
+class Example implements ExampleInterface
+{
+    /** {@inheritdoc} */
+    public function doSomething(\stdClass $object)
+    {
+        // what ever
+        return true;
+    }
+}
+
+interface DeepExampleInterface extends ExampleInterface
+{
+    /**
+     * Convert $json to object and doSomething
+     *
+     * @param string $json
+     * @return bool
+     */
+    public function doSomethingJson($json);
+}
+
+class DeepExample extends Example implements DeepExampleInterface
+{
+    /** {@inheritdoc} */
+    public function doSomethingJson($json)
+    {
+        return $this->doSomething(json_encode($json));
+    }
+
+    /** {@inheritdoc} */
+    public function doSomething(\stdClass $object)
+    {
+        // do something really special
+        return true;
+    }
+}
