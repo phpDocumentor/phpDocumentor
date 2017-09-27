5934a3c86679755d7b0e5a42ef8405d8640e038a
Marieke Bednarczyk <mbed@live.nl>
2017-09-27 16:33:30 +0200

    Make sure Fqsen's and Url's are handled correctly in StandardRouter
    Due to refactoring the reference in the see tags now points to an fqsen object instead of a descriptor.
    These could not be handled yet in the StandardRouter which caused the urls to go wrong in the output.

---
+++ src/phpDocumentor/Transformer/Router/UrlGenerator/Standard/FqsenDescriptor.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.3
+ *
+ * @copyright 2010-2014 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Transformer\Router\UrlGenerator\Standard;
+
+use phpDocumentor\Reflection\DocBlock\Tags\Reference\Fqsen;
+use phpDocumentor\Transformer\Router\UrlGenerator\UrlGeneratorInterface;
+
+/**
+ * Generates a relative URL with properties for use in the generated HTML documentation.
+ */
+class FqsenDescriptor implements UrlGeneratorInterface
+{
+    /**
+     * Generates a URL from the given node or returns false if unable.
+     *
+     * @param string|Fqsen $node
+     *
+     * @return string|false
+     */
+    public function __invoke($node)
+    {
+        if (!($node instanceof Fqsen)) {
+            return false;
+        }
+
+        $converter = new QualifiedNameToUrlConverter();
+        $fqsenParts = explode('::', (string)$node);
+
+        $className = $fqsenParts[0];
+
+        if (count($fqsenParts) === 1) {
+            return '/classes/' . $converter->fromClass($className) . '.html';
+        }
+
+        if (strpos($fqsenParts[1], '$') !== false) {
+            $propertyName = explode('$', $fqsenParts[1]);
+            return '/classes/' . $converter->fromClass($className) . '.html#property_' . $propertyName[1];
+        }
+
+        if (strpos($fqsenParts[1], '()') !== false) {
+            $methodName = explode('()', $fqsenParts[1]);
+            return '/classes/' . $converter->fromClass($className) . '.html#method_' . $methodName[0];
+        }
+
+        return '/classes/' . $converter->fromClass($className) . '.html#constant_' . $fqsenParts[1];
+    }
+}
---
+++ tests/unit/phpDocumentor/Transformer/Router/UrlGenerator/Standard/FqsenDescriptorTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.3
+ *
+ * @copyright 2010-2014 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Transformer\Router\UrlGenerator\Standard;
+
+use Mockery as m;
+use phpDocumentor\Reflection\DocBlock\Tags\Reference\Fqsen;
+use phpDocumentor\Reflection\Fqsen as RealFqsen;
+
+/**
+ * Test for the MethodDescriptor URL Generator with the Standard Router
+ */
+class FqsenDescriptorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers phpDocumentor\Transformer\Router\UrlGenerator\Standard\FqsenDescriptor::__invoke
+     * @covers phpDocumentor\Transformer\Router\UrlGenerator\Standard\QualifiedNameToUrlConverter::fromClass
+     * @dataProvider provideFqsens
+     */
+    public function testGenerateUrlForFqsenDescriptor($fromFqsen, $toPath)
+    {
+        // Arrange
+        $realFqsen = new RealFqsen($fromFqsen);
+        $fqsen = new Fqsen($realFqsen);
+        $fixture = new FqsenDescriptor();
+
+        // Act
+        $result = $fixture($fqsen);
+
+        // Assert
+        $this->assertSame($toPath, $result);
+    }
+
+    /**
+     * @covers phpDocumentor\Transformer\Router\UrlGenerator\Standard\FqsenDescriptor::__invoke
+     */
+    public function testFqsenDescriptorReturnsFalseWhenNodeOfWrongType()
+    {
+        // Arrange
+        $fqsen = m::mock('phpDocumentor\Reflection\DocBlock\Tags\Reference\Reference');
+        $fixture = new FqsenDescriptor();
+
+        // Act
+        $result = $fixture($fqsen);
+
+        // Assert
+        $this->assertSame(false, $result);
+    }
+
+    public function provideFqsens()
+    {
+        return array(
+            array('\\My\\Space\\Class', '/classes/My.Space.Class.html'),
+            array('\\My\\Space\\Class::$property', '/classes/My.Space.Class.html#property_property'),
+            array('\\My\\Space\\Class::method()', '/classes/My.Space.Class.html#method_method'),
+            array('\\My\\Space\\Class::CONSTANT', '/classes/My.Space.Class.html#constant_CONSTANT'),
+        );
+    }
+}
