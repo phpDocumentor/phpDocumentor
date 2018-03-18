95ee6b707d23f6ba1483cd8d73e37921398f4d32
Jaapio <jaap@ijaap.nl>
2018-03-18 21:56:36 +0100

    Adds deprecation messages

--- src/phpDocumentor/Application/CilexCompatibilityLayer/Container.php
+++ src/phpDocumentor/Application/CilexCompatibilityLayer/Container.php
@@ -21,11 +21,23 @@ if (!class_exists(\Pimple\Container::class, false)) {
 
         public function offsetExists($offset)
         {
+            trigger_error(
+                'Usage of pimple container is deprecated and will be remove in v4,
+                 please use the symfony service wiring.',
+                E_USER_DEPRECATED
+            );
+
             return $this->container->has($offset);
         }
 
         public function offsetGet($offset)
         {
+            trigger_error(
+                'Usage of pimple container is deprecated and will be remove in v4,
+                 please use the symfony service wiring.',
+                E_USER_DEPRECATED
+            );
+
             try {
                 return $this->container->get($offset);
             } catch (NotFoundExceptionInterface $exception) {
@@ -35,6 +47,12 @@ if (!class_exists(\Pimple\Container::class, false)) {
 
         public function offsetSet($offset, $value)
         {
+            trigger_error(
+                'Usage of pimple container is deprecated and will be remove in v4,
+                 please use the symfony service wiring.',
+                E_USER_DEPRECATED
+            );
+
             if ($value instanceof \Closure) {
                 $value = $value($this);
             }
@@ -43,21 +61,46 @@ if (!class_exists(\Pimple\Container::class, false)) {
 
         public function offsetUnset($offset)
         {
+            trigger_error(
+                'Usage of pimple container is deprecated and will be remove in v4,
+                 please use the symfony service wiring.',
+                E_USER_DEPRECATED
+            );
+
             $this->container->set($offset, null);
         }
 
         public function register(ServiceProviderInterface $serviceProvider)
         {
+            trigger_error(
+                'Usage of pimple container is deprecated and will be remove in v4,
+                 please use the symfony service wiring.',
+                E_USER_DEPRECATED
+            );
+
             $serviceProvider->register($this);
         }
 
         public function extend(string $serviceId, \Closure $extendingService)
         {
+            trigger_error(
+                'Usage of pimple container is deprecated and will be remove in v4,
+                 please use the symfony service wiring.',
+                E_USER_DEPRECATED
+            );
+
             return $extendingService($this->container->get($serviceId));
         }
 
         public function command(Command $command)
         {
+            trigger_error(
+                'Usage of pimple container is deprecated and will be remove in v4,
+                 please use the symfony service wiring.',
+                E_USER_DEPRECATED
+            );
+
+
             if ( ! $this->container->has('phpdocumentor.compatibility.extra_commands')) {
                 $this->container->set('phpdocumentor.compatibility.extra_commands', new \ArrayObject());
             }
