4aa3cdb07b4e2feb96e1d80fb03e1b8ce6fd7377
Jaapio <jaap@ijaap.nl>
2018-07-29 20:30:43 +0200

    Adds unittest for application

---
+++ tests/unit/phpDocumentor/Application/Console/ApplicationTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Mike van Riel <mike.vanriel@naenius.com>
+ * @copyright 2010-2018 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ *
+ *
+ */
+
+namespace phpDocumentor\Application\Console;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery as m;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class ApplicationTest extends MockeryTestCase
+{
+    public function testGetLongVersion(): void
+    {
+        $kernelMock = m::mock(KernelInterface::class);
+        $kernelMock->shouldIgnoreMissing();
+        $feature = new Application($kernelMock);
+
+       self::assertRegExp('~phpDocumentor <info>v(\d).(\d).(\d|x)?-(.*)</info>~',  $feature->getLongVersion());
+    }
+}
