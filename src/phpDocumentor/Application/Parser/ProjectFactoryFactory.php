7c4c3083a3e29f206585b37fe1abac1f17e1e863
Mike van Riel <me@mikevanriel.com>
2018-09-03 16:56:50 +0200

    Ensure Parser middleware are executed in the correct order
    Since the middlewares are expected to be in a specific order I have defined
    each I know and assigned a priority that will ensure their order. I have spaced
    these apart by 1000 so that any custom middlewares can easily be fit between
    them if need be.

--- src/phpDocumentor/Application/Parser/ProjectFactoryFactory.php
+++ src/phpDocumentor/Application/Parser/ProjectFactoryFactory.php
@@ -30,8 +30,6 @@ final class ProjectFactoryFactory
             $fileMiddlewares[] = $middelware;
         }
 
-        $fileMiddlewares = array_reverse($fileMiddlewares);
-
         $strategies = [
             new Factory\Argument(new PrettyPrinter()),
             new Factory\Class_(),
