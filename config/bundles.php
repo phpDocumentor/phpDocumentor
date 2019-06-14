204b7194fe8eb2edb15a412472d26be30a353fea
Mike van Riel <me@mikevanriel.com>
2019-06-14 19:42:14 +0200

    Remove more unused packages

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
