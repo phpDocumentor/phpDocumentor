b970d52377105b6bb8ee5d02986ba21fd40860b2
Mike van Riel <mike@ingewikkeld.net>
2020-01-02 22:00:33 +0100

    Add more tests for the namespace detail page

--- cypress/integration/namespace-details.spec.js
+++ cypress/integration/namespace-details.spec.js
@@ -31,35 +31,63 @@ describe('Namespace Detail Page', function() {
     });
 
     it('Has a section "Interfaces and Classes" featuring the "Pizzeria" class', function() {
-        cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes");
-        cy.get('.phpdocumentor-content > h3 + dl dt a').contains("Pizzeria");
+        cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes")
+            .next().get('dt a').contains("Pizzeria");
     });
 
     it('Has a section "Interfaces and Classes" featuring the "Product" interface', function() {
-        cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes");
-        cy.get('.phpdocumentor-content > h3 + dl dt a').contains("Product");
+        cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes")
+            .next().get('dt a').contains("Product");
     });
 
     it('Goes to "Pizzeria" its detail page when you click on it', function() {
-        cy.get('.phpdocumentor-content > h3 + dl dt a').contains("Pizzeria").click();
+        cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes")
+            .next().get('dt a').contains("Pizzeria").click();
+
         cy.url().should('include', '/classes/Marios-Pizzeria.html');
         cy.get('.phpdocumentor-content > h2').contains("Pizzeria");
     });
 
     it('Has a section "Functions" featuring the "heatOven" function', function() {
-        cy.get('.phpdocumentor-content > h3').contains("Functions");
-        cy.get('.phpdocumentor-content > h3 + ul li a').contains("heatOven");
+        cy.get('.phpdocumentor-content > h3').contains("Functions")
+            .next().get('li a').contains("heatOven");
     });
 
     // TODO: Test shows broken behaviour, fix that
     it.skip('Has a section "Constants" featuring the "OVEN_TEMPERATURE" constant', function() {
-        cy.get('.phpdocumentor-content > h3').contains("Constants");
-        cy.get('.phpdocumentor-content > h3 + ul li a').contains("OVEN_TEMPERATURE");
+        cy.get('.phpdocumentor-content > h3').contains("Constants")
+            .next().get('li a').contains("OVEN_TEMPERATURE");
     });
 
     // TODO: Test shows broken behaviour, fix that
     it.skip('Has a section "Constants" featuring the "HIGHER_OVEN_TEMPERATURE" constant', function() {
-        cy.get('.phpdocumentor-content > h3').contains("Constants");
-        cy.get('.phpdocumentor-content > h3 + ul li a').contains("HIGHER_OVEN_TEMPERATURE");
+        cy.get('.phpdocumentor-content > h3').contains("Constants")
+            .next().get('li a').contains("HIGHER_OVEN_TEMPERATURE");
+    });
+});
+
+describe('Namespace Detail Page for a (sub)namespace', function() {
+    beforeEach(function(){
+        cy.visit('data/examples/MariosPizzeria/build/api/namespaces/marios-pizza.html');
+    });
+
+    it('Has "Pizza" as title', function() {
+        cy.get('.phpdocumentor-content > h2').contains("Pizza");
+    });
+
+    it('Has a breadcrumb featuring "Home" and "Marios"', function() {
+        cy.get('.phpdocumentor-breadcrumbs').contains("Home");
+        cy.get('.phpdocumentor-breadcrumbs').contains("Marios");
+        cy.get('.phpdocumentor-breadcrumbs > li').should('have.length', 2);
+    });
+
+    it('will send you to the index when clicking on "Home" in the breadcrumb', function() {
+        cy.get('.phpdocumentor-breadcrumbs').contains("Home").click();
+        cy.url().should('include', '/index.html');
+    });
+
+    it('Has a section "Interfaces and Classes" featuring the "Base" class', function() {
+        cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes")
+            .next().get('dt a').contains("Base");
     });
 });
