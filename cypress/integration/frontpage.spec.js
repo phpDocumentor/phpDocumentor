bb15a27cbebf81bd06f021b99932512eed42a7e2
Mike van Riel <mike@ingewikkeld.net>
2020-01-02 18:35:32 +0100

    Add initial e2e test for Default template
    Because Behat is not able to comfortably test frontends, I have done an
    experiment using Cypress as a test runner. This change is the result of
    this experiment.
    
    In this change I have introduced a new make command `make test-template`
    that will install Cypress using NPM, run phpDocumentor against the
    Mario's Pizzeria example, and test the output using Cypress.
    
    I have chosen not to include this in the build pipeline yet as this is
    an example meant for feedback; if we continue this way I want to clean
    up the make commands a bit more and add it to Github Actions.

---
+++ cypress/integration/frontpage.spec.js
@@ -0,0 +1,18 @@
+describe('Frontpage', function() {
+    before(function(){
+        cy.visit('tests/features/assets/projects/MariosPizzeria/build/api/index.html');
+    });
+
+    it('Has "Documentation" as title', function() {
+        cy.get('.phpdocumentor-title').contains("Documentation");
+    });
+
+    it('Has a search bar', function() {
+        cy.get('.phpdocumentor-sidebar__category-header').contains("Search");
+        cy.get('.phpdocumentor-field.phpdocumentor-search__field');
+    });
+
+    it('The "Marios" namespace in the sidebar', function() {
+        cy.get('.phpdocumentor-sidebar').contains("Marios");
+    });
+});
