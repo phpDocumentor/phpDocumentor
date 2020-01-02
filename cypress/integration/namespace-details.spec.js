de32ddfb309f99787be103f0514c7ad41147c6c7
Mike van Riel <mike@ingewikkeld.net>
2020-01-02 21:41:30 +0100

    Wrote more tests for the Namespace details

--- cypress/integration/namespace-details.spec.js
+++ cypress/integration/namespace-details.spec.js
@@ -17,17 +17,25 @@ describe('Namespace Detail Page', function() {
         cy.url().should('include', '/index.html');
     });
 
-    it('Has a section Namespaces featuring the "Pizza" sub-namespace', function() {
-        cy.get('.phpdocumentor-content > h3').contains("Namespaces");
-        cy.get('.phpdocumentor-content > h3 + ul li a').contains("Pizza");
+    it('Has a section "Namespaces" featuring the "Pizza" sub-namespace', function() {
+        cy.get('.phpdocumentor-content > h3').contains("Namespaces")
+            .next().get('li a').contains("Pizza");
     });
 
-    it('Has a section classes and interfaces featuring the "Pizzeria" class', function() {
+    it('Goes to the "Pizza" sub-namespace when you click on it', function() {
+        cy.get('.phpdocumentor-content > h3').contains("Namespaces")
+            .next().get('li a').contains("Pizza").click();
+
+        cy.url().should('include', '/namespaces/marios-pizza.html');
+        cy.get('.phpdocumentor-content > h2').contains("Pizza");
+    });
+
+    it('Has a section "Interfaces and Classes" featuring the "Pizzeria" class', function() {
         cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes");
         cy.get('.phpdocumentor-content > h3 + dl dt a').contains("Pizzeria");
     });
 
-    it('Has a section classes and interfaces featuring the "Product" interface', function() {
+    it('Has a section "Interfaces and Classes" featuring the "Product" interface', function() {
         cy.get('.phpdocumentor-content > h3').contains("Interfaces and Classes");
         cy.get('.phpdocumentor-content > h3 + dl dt a').contains("Product");
     });
@@ -36,5 +44,22 @@ describe('Namespace Detail Page', function() {
         cy.get('.phpdocumentor-content > h3 + dl dt a').contains("Pizzeria").click();
         cy.url().should('include', '/classes/Marios-Pizzeria.html');
         cy.get('.phpdocumentor-content > h2').contains("Pizzeria");
+    });
+
+    it('Has a section "Functions" featuring the "heatOven" function', function() {
+        cy.get('.phpdocumentor-content > h3').contains("Functions");
+        cy.get('.phpdocumentor-content > h3 + ul li a').contains("heatOven");
+    });
+
+    // TODO: Test shows broken behaviour, fix that
+    it.skip('Has a section "Constants" featuring the "OVEN_TEMPERATURE" constant', function() {
+        cy.get('.phpdocumentor-content > h3').contains("Constants");
+        cy.get('.phpdocumentor-content > h3 + ul li a').contains("OVEN_TEMPERATURE");
+    });
+
+    // TODO: Test shows broken behaviour, fix that
+    it.skip('Has a section "Constants" featuring the "HIGHER_OVEN_TEMPERATURE" constant', function() {
+        cy.get('.phpdocumentor-content > h3').contains("Constants");
+        cy.get('.phpdocumentor-content > h3 + ul li a').contains("HIGHER_OVEN_TEMPERATURE");
     });
 });
---
+++ data/examples/MariosPizzeria/src/constants.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Marios;
+
+const OVEN_TEMPERATURE = 9001;
+define('HIGHER_OVEN_TEMPERATURE', 9002);
