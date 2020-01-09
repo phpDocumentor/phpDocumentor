4e42536d3339f2524a494bb257e5ed0149d70f27
Mike van Riel <mike@ingewikkeld.net>
2020-01-09 20:09:08 +0100

    Add support for 'deprecated' and test various method modifiers

--- cypress/integration/default/class-details.spec.js
+++ cypress/integration/default/class-details.spec.js
@@ -48,7 +48,10 @@ describe('Class Detail Page', function() {
 
     describe('Showing a method in a class', function() {
         let methods;
-        before(function(){
+
+        const findMethod = (name) => methods.get('.phpdocumentor-method__name').contains(name).parent();
+
+         before(function(){
             cy.visit('build/default/classes/Marios-Pizzeria.html');
             methods = cy.get('.phpdocumentor-method');
         });
@@ -58,10 +61,11 @@ describe('Class Detail Page', function() {
             method.get('.phpdocumentor-method-signature__argument__variadic-operator').contains('...');
         });
 
-        describe('Shows the "jsonSerialize" implemented method from the "JsonSerializable" interface', function () {
+        describe('Shows the "jsonSerialize" method; as an example of a public, implemented, method from the "JsonSerializable" interface', function () {
             let method;
+
             before(function(){
-                method = methods.get('.phpdocumentor-method__name').contains('jsonSerialize()').parent();
+                method = findMethod('jsonSerialize()');
             });
 
             it('Shows the name "jsonSerialize()"', function() {
@@ -83,15 +87,27 @@ describe('Class Detail Page', function() {
                 method.get('.phpdocumentor-description').should('not.exist');
             });
 
+            it('Has a "public" visibility class to influence styling', function() {
+                method.should('have.class', '.phpdocumentor-method---public');
+                method.should('not.have.class', '.phpdocumentor-method---protected');
+                method.should('not.have.class', '.phpdocumentor-method---private');
+            });
+
+            it('Is not marked as static, final or abstract', function() {
+                method.should('not.have.class', '.phpdocumentor-method---final');
+                method.should('not.have.class', '.phpdocumentor-method---static');
+                method.should('not.have.class', '.phpdocumentor-method---abstract');
+            });
+
             describe('signature', function () {
                 let signature;
 
                 beforeEach(function(){
-                    signature = method.get('phpdocumentor-method-signature');
+                    signature = method.get('.phpdocumentor-method-signature');
                 });
 
                 it('Shows the "public" visibility specifier', function() {
-                    signature.get('phpdocumentor-method-signature__visibility').contains('public');
+                    signature.get('.phpdocumentor-method-signature__visibility').contains('public');
                 });
 
                 it('Shows the name of the method', function() {
@@ -100,6 +116,101 @@ describe('Class Detail Page', function() {
 
                 it('Shows the "array" return value', function() {
                     signature.get('.phpdocumentor-method-signature__argument__return-type').contains('array');
+                });
+            });
+        });
+
+        describe('Shows "doOrder" as an example of a protected static method with summary and description', function () {
+            let method;
+
+            before(function(){
+                method = findMethod('doOrder()');
+            });
+
+            it('Shows the name "doOrder()"', function() {
+                method.get('.phpdocumentor-method__name').contains("doOrder()");
+            });
+
+            it('Has a "protected private" visibility class to influence styling', function() {
+                method.should('not.have.class', '.phpdocumentor-method---public');
+                method.should('have.class', '.phpdocumentor-method---protected');
+                method.should('not.have.class', '.phpdocumentor-method---private');
+            });
+
+            it('Is marked as static, but not as final, deprecated or abstract', function() {
+                method.should('not.have.class', '.phpdocumentor-method---final');
+                method.should('have.class', '.phpdocumentor-method---static');
+                method.should('not.have.class', '.phpdocumentor-method---abstract');
+                method.should('not.have.class', '.phpdocumentor-method---deprecated');
+            });
+
+            describe('signature', function () {
+                let signature;
+
+                beforeEach(function(){
+                    signature = method.get('.phpdocumentor-method-signature');
+                });
+
+                it('Shows the "protected" visibility specifier', function() {
+                    signature.get('.phpdocumentor-method-signature__visibility').contains('protected');
+                });
+
+                it('Shows the "static" keyword', function() {
+                    signature.get('.phpdocumentor-method-signature__static').contains('static');
+                });
+
+                it('Shows "array" as return value', function() {
+                    signature.get('.phpdocumentor-method-signature__argument__return-type').contains('array');
+                });
+            });
+        });
+
+        describe('Shows "doOldOrder" as an example of a deprecated private final method', function () {
+            let method;
+
+            before(function(){
+                method = findMethod('doOldOrder()');
+            });
+
+            it('Shows the name "doOldOrder()"', function() {
+                method.get('.phpdocumentor-method__name').contains("doOldOrder()");
+            });
+
+            it('Has a "private" visibility class to influence styling', function() {
+                method.should('not.have.class', '.phpdocumentor-method---public');
+                method.should('not.have.class', '.phpdocumentor-method---protected');
+                method.should('have.class', '.phpdocumentor-method---private');
+            });
+
+            it('Is marked as final and deprecated, but not as static or abstract', function() {
+                method.should('have.class', '.phpdocumentor-method---final');
+                method.should('not.have.class', '.phpdocumentor-method---static');
+                method.should('not.have.class', '.phpdocumentor-method---abstract');
+                method.should('have.class', '.phpdocumentor-method---deprecated');
+            });
+
+            describe('signature', function () {
+                let signature;
+
+                beforeEach(function(){
+                    signature = method.get('.phpdocumentor-method-signature');
+                });
+
+                it('has the deprecated modifier', function() {
+                    signature.should('have.class', '.phpdocumentor-method-signature--deprecated');
+                });
+
+                it('Shows the "private" visibility specifier', function() {
+                    signature.get('.phpdocumentor-method-signature__visibility').contains('private');
+                });
+
+                it('Shows the "final" keyword', function() {
+                    signature.get('.phpdocumentor-method-signature__final').contains('final');
+                });
+
+                it('Shows "false" as return value; the return tag overrides the typehint', function() {
+                    // @todo fix this; this should have failed!!!
+                    signature.get('.phpdocumentor-method-signature__argument__return-type').contains('false');
                 });
             });
         });
--- data/examples/MariosPizzeria/src/Pizzeria.php
+++ data/examples/MariosPizzeria/src/Pizzeria.php
@@ -17,6 +17,32 @@ final class Pizzeria implements \JsonSerializable
     }
 
     /**
+     * Places an order for a pizza.
+     *
+     * This is an example of a protected function with the static modifier whose parameters' type and return type is
+     * determined by the DocBlock and no type hints are given in the method signature.
+     *
+     * @param Pizza $pizza The specific pizza to place an order for.
+     *
+     * @return bool Whether the order succeeded
+     */
+    protected static function doOrder($pizza)
+    {
+        return true;
+    }
+
+    /**
+     * @deprecated This ordering method should no longer be used; it will always fail.
+     *
+     * @return false Demonstrate that 'false' is a valid return type in an DocBlock to indicate it won't just return any
+     *      boolean; it will _always_ be false.
+     */
+    private final function doOldOrder(): bool
+    {
+        return false;
+    }
+
+    /**
      * @inheritDoc
      */
     public function jsonSerialize(): array
