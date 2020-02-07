cf2e4cff1e8a1ab5e5bb7d0bcb8120e95ce6ce4e
Orklah <ronan_morin@hotmail.com>
2020-02-07 23:20:06 +0100

    fix CS

--- src/phpDocumentor/Descriptor/Collection.php
+++ src/phpDocumentor/Descriptor/Collection.php
@@ -232,16 +232,17 @@ class Collection implements Countable, IteratorAggregate, ArrayAccess
     }
 
     /**
-     * @template C
-     *
      * @param class-string<C> $classString
      * @param array<C> $elements
      *
      * @return Collection<C>
+     *
+     * @template C
      */
     public static function fromClassString(string $classString, array $elements = []) : Collection
     {
         Assert::classExists($classString);
+
         return new Collection($elements);
     }
 }
