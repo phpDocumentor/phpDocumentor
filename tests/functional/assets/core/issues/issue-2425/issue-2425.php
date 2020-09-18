268050f03461dfee9d039797460b51ba4b20343e
Jaapio <jaap@ijaap.nl>
2020-09-18 21:47:45 +0200

    Add testcase for issue #2425
    Issue was fixed by a pr on DocblockReflection. This patch
    adds a testcase to prove the issue is fixed by now.
    
    fixes #2425

---
+++ tests/functional/assets/core/issues/issue-2425/issue-2425.php
@@ -0,0 +1,16 @@
+<?php
+namespace Project\Sub\Level;
+
+use Project\Other\Level\Issue2425B;
+use Project\Other\Level\Issue2425C as Aliased;
+
+/**
+ * Class summary.
+ *
+ * A description containing an inline {@see Issue2425B::bar()} tag
+ * to a class inside of the project referenced via a use statement.
+ *
+ * And here is another inline {@see Aliased::bar()} tag to a class
+ * aliased via a use statement.
+ */
+class Issue2425A {}
