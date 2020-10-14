5eecacd2d238f3ec3619b8124eaab7e447abe91f
orklah <ronan_morin@hotmail.com>
2020-10-14 14:25:41 +0200

    fix build

--- src/phpDocumentor/Compiler/Compiler.php
+++ src/phpDocumentor/Compiler/Compiler.php
@@ -19,7 +19,8 @@ use Webmozart\Assert\Assert;
 /**
  * Contains a series of compiler steps in a specific order; ready to be executed during transformation.
  *
- * @template-extends SplPriorityQueue<CompilerPassInterface, int>
+ * @psalm-template-extends SplPriorityQueue<CompilerPassInterface, int>
+ * @template-extends SplPriorityQueue<int, CompilerPassInterface>
  */
 class Compiler extends SplPriorityQueue
 {
