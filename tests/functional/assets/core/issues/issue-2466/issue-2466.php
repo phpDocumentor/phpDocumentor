2913d384a8acd8f59d2feddbb084ff49192dfead
Jeff Horton <jeff@goblinoid.co.uk>
2020-10-27 22:32:03 +0000

    Fix #2466 - replace twig variables with the updated names - add a functional test for the xml out put containing the expected outputs

---
+++ tests/functional/assets/core/issues/issue-2466/issue-2466.php
@@ -0,0 +1,17 @@
+<?php
+    
+    /**
+     * Class Issue2466
+     *
+     */
+    class Issue2466
+    {
+        /**
+         * This is a description of the property
+         *
+         * Here is more detailed explanation of what this property is for.
+         *
+         * @var string
+         */
+        public $testProperty = 'default';
+    }
