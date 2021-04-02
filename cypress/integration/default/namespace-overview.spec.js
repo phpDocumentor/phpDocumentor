9956aa89ae5fb2d19d081ff15cf33938756a1acd
Mike van Riel <me@mikevanriel.com>
2021-04-02 14:12:32 +0200

    Add more asserts to Namespace E2E test and setup Cypress Dashboard

--- cypress/integration/default/namespace-overview.spec.js
+++ cypress/integration/default/namespace-overview.spec.js
@@ -38,47 +38,98 @@ describe('Namespace Overview', function() {
         });
     })
 
-    describe('contains a list of interfaces, classes and traits', function() {
-        it('Has a section "Interfaces, Classes and Traits" featuring the "Pizzeria" class', function() {
+    describe('The list of interfaces, classes and traits', function() {
+        const sectionTitle = 'Interfaces, Classes and Traits';
+
+        it('Has a section "Interfaces, Classes and Traits" with a table of contents', function() {
             cy.get('h3#interfaces_class_traits')
-                .contains("Interfaces, Classes and Traits")
-                .next('.phpdocumentor-table-of-contents')
-                .find('.phpdocumentor-table-of-contents__entry')
-                .should('contain', 'Pizzeria');
+                .contains(sectionTitle)
+                .next('.phpdocumentor-table-of-contents');
         });
 
-        it('Has a section "Interfaces, Classes and Traits" featuring the "Product" interface', function() {
+        it('Features the "Product" interface', function() {
+            const name = 'Product';
+
             cy.get('h3#interfaces_class_traits')
-                .contains("Interfaces, Classes and Traits")
+                .contains(sectionTitle)
                 .next('.phpdocumentor-table-of-contents')
-                .find('.phpdocumentor-table-of-contents__entry')
-                .should('contain', 'Product');
+                .contains('.phpdocumentor-table-of-contents__entry', name)
+                .should('have.class', '-interface');
+        });
+
+        it('Features the "Pizzeria" class and its description', function() {
+            const title = 'Pizzeria';
+            const description = 'Entrypoint for this pizza ordering application.';
+
+            cy.get('h3#interfaces_class_traits')
+                .contains(sectionTitle)
+                .next('.phpdocumentor-table-of-contents')
+                .contains('.phpdocumentor-table-of-contents__entry', title)
+                .should('have.class', '-class')
+                .next('dd')
+                .should('have.text', description)
         });
 
         it('Goes to "Pizzeria" its detail page when you click on it', function() {
+            const title = 'Pizzeria';
+
             cy.get('h3#interfaces_class_traits')
-                .contains("Interfaces, Classes and Traits")
+                .contains(sectionTitle)
                 .next('.phpdocumentor-table-of-contents')
-                .find('.phpdocumentor-table-of-contents__entry')
-                .contains("Pizzeria").click();
+                .contains('.phpdocumentor-table-of-contents__entry a', title)
+                .click();
 
-            shouldVisitPageWithTitle('/classes/Marios-Pizzeria.html', 'Pizzeria');
+            shouldVisitPageWithTitle('/classes/Marios-Pizzeria.html', title);
+        });
+
+        it('Features the "SharedTrait" trait with its description', function() {
+            const name = 'SharedTrait';
+
+            cy.get('h3#interfaces_class_traits')
+                .contains(sectionTitle)
+                .next('.phpdocumentor-table-of-contents')
+                .contains('.phpdocumentor-table-of-contents__entry', name)
+                .should('have.class', '-trait')
+                .next('dd')
+                .should('have.text', 'Trait that all pizza\'s could share.');
         });
     });
 
-    describe('contains a section with the details of the functions in this namespace', function() {
-        it('Has a section "Functions" featuring the "heatOven" function', function () {
-            cy.get('h3#functions').contains("Functions")
-                .siblings('.phpdocumentor-element.-function')
-                .contains("heatOven");
+    describe('Constants section', function() {
+        let sectionTitle = 'Constants';
+
+        it('Shows the title "Constants"', function () {
+            cy.get('h3#constants')
+                .contains(sectionTitle);
+        });
+
+        it('Features the "OVEN_TEMPERATURE" constant', function () {
+            let name = 'OVEN_TEMPERATURE';
+
+            cy.get('h3#constants')
+                .contains(sectionTitle)
+                .siblings()
+                .contains('.phpdocumentor-element', name)
+                .should('have.class', '-constant');
         });
     });
 
-    describe('contains a section with the details of the constants in this namespace', function() {
-        it('Has a section "Constants" featuring the "OVEN_TEMPERATURE" constant', function () {
-            cy.get('h3#constants').contains("Constants")
-                .siblings('.phpdocumentor-element.-constant')
-                .contains("OVEN_TEMPERATURE");
+    describe('Functions section', function() {
+        let sectionTitle = 'Functions';
+
+        it('Shows the title "Functions"', function () {
+            cy.get('h3#functions')
+                .contains(sectionTitle);
+        });
+
+        it('features the "heatOven" function', function () {
+            let title = 'heatOven';
+
+            cy.get('h3#functions')
+                .contains(sectionTitle)
+                .siblings()
+                .contains('.phpdocumentor-element', title)
+                .should('have.class', '-function');
         });
     });
 });
