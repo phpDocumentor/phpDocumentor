ec35883ba0ca3e19a7dd3627663e4c13281a8845
Jaapio <jaap@ijaap.nl>
2021-08-10 20:04:17 +0200

    Invert logic to early continue

--- src/phpDocumentor/Kernel.php
+++ src/phpDocumentor/Kernel.php
@@ -89,9 +89,11 @@ class Kernel extends BaseKernel
     {
         $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
-            if (isset($envs['all']) || isset($envs[$this->environment])) {
-                yield new $class();
+            if (isset($envs['all']) === false && isset($envs[$this->environment]) === false) {
+                continue;
             }
+
+            yield new $class();
         }
     }
 
