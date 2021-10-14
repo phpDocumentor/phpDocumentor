f10fce8f3ad8874f2a2f245ec66580f40fe78d3a
Mike van Riel <me@mikevanriel.com>
2021-10-14 22:43:24 +0200

    Migrated service loading to a bundle
    Because we cannot use relative paths in the original service.yaml when
    you include the project using composer (which you shouldn't .. anyway) I
    needed to actually make a bundle out of it with a DI Extension

---
+++ incubator/guides/src/DependencyInjection/phpDocumentorGuidesExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class phpDocumentorGuidesExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yaml');
+    }
+}
---
+++ incubator/guides/src/phpDocumentorGuidesBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class phpDocumentorGuidesBundle extends Bundle
+{
+
+}
