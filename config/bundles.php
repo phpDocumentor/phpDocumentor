7e82ed3eb927442d7da0e2e345f3164a457605f5
Mike van Riel <me@mikevanriel.com>
2021-10-16 09:15:31 +0200

    Re-integrate service definitions of Guides
    After discussing this with Jaap, I have re-integrated the Guides service
    definitions of the libraries back into the main application's service
    definitions.
    
    In this process I have split the service definitions for Guides into a
    separate definition file so that it still remains grouped.

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,4 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
-    phpDocumentor\Guides\phpDocumentorGuidesBundle::class => ['all' => true],
-    phpDocumentor\Guides\phpDocumentorGuidesRestructuredTextBundle::class => ['all' => true],
-    phpDocumentor\Guides\phpDocumentorGuidesMarkdownBundle::class => ['all' => true],
 ];
