26e65d84abed6a3099d9a2c2bc65da7d930607f7
Jaapio <jaap@ijaap.nl>
2021-11-02 20:58:27 +0100

    Add directoryloader test

---
+++ tests/unit/phpDocumentor/Extension/DirectoryLoaderTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Extension;
+
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \phpDocumentor\Extension\DirectoryLoader
+ */
+final class DirectoryLoaderTest extends TestCase
+{
+    /**
+     * @covers ::supports
+     * @covers ::findManifestFile
+     */
+    public function testSupportsReturnsFalseWhenNoManifestFileFoundInEmptyDir(): void
+    {
+        $fileSystem = vfsStream::setup('extensions');
+        $fileSystem->addChild(vfsStream::newDirectory('myExtension'));
+
+        $loader = new DirectoryLoader();
+        self::assertFalse($loader->supports(new \DirectoryIterator($fileSystem->url())));
+    }
+
+    /**
+     * @covers ::supports
+     * @covers ::findManifestFile
+     */
+    public function testSupportsReturnsFalseWhenNoManifestFileFound(): void
+    {
+        $fileSystem = vfsStream::setup('extensions');
+        $fileSystem->addChild(vfsStream::newFile('myExtension'));
+
+        $loader = new DirectoryLoader();
+        self::assertFalse($loader->supports(new \DirectoryIterator($fileSystem->url())));
+    }
+
+    /**
+     * @covers ::supports
+     * @covers ::findManifestFile
+     */
+    public function testSupportsReturnsTrueWhenManifestFileFound(): void
+    {
+        $fileSystem = vfsStream::setup('extensions');
+        $fileSystem->addChild(vfsStream::newFile('manifest.xml'));
+
+        $loader = new DirectoryLoader();
+        self::assertTrue($loader->supports(new \DirectoryIterator($fileSystem->url())));
+    }
+
+    /**
+     * @covers ::load
+     * @covers ::findManifestFile
+     */
+    public function testNotSupportedDirectoryReturnsNullOnLoad(): void
+    {
+        $fileSystem = vfsStream::setup('extensions');
+        $fileSystem->addChild(vfsStream::newFile('myExtension'));
+
+        $loader = new DirectoryLoader();
+        self::assertNull($loader->load(new \DirectoryIterator($fileSystem->url())));
+    }
+
+    /**
+     * @covers ::load
+     * @covers ::findManifestFile
+     */
+    public function testExtensionIsCreatedWhenExtensionDirValid(): void
+    {
+        $fileSystem = vfsStream::copyFromFileSystem(__DIR__ . '/../../../data/extensions/example');
+
+        $loader = new DirectoryLoader();
+        $extension = $loader->load(new \DirectoryIterator($fileSystem->url()));
+
+        self::assertInstanceOf(Extension::class, $extension);
+    }
+}
