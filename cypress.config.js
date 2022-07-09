70d42f861a3992adba347e64f8eea989689fe71a
Mike van Riel <me@mikevanriel.com>
2022-07-09 10:51:17 +0200

    Make necessary changes to upgrade to Cypress 10

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    e2e:{
+        projectId: "ifc328",
+        supportFile: false,
+        specPattern: "**/*.spec.js",
+        retries: 2,
+    }
+})
