e96eab9f1deba91e003d6d6d5ae420ec843f0618
Mike van Riel <me@mikevanriel.com>
2022-07-23 12:35:37 +0200

    Add phpDocumentor version to cache namespace
    Instead of remembering the cached version, we can namespace the cache
    paths based on phpDocumentor's version. By changing the TTL to 1 month
    and not changing the path but instead changing the namespace based on
    the version, old caches should be pruned after a month.

--- src/phpDocumentor/Parser/Cache/FilesystemAdapter.php
+++ src/phpDocumentor/Parser/Cache/FilesystemAdapter.php
@@ -13,12 +13,17 @@ declare(strict_types=1);
 
 namespace phpDocumentor\Parser\Cache;
 
+use phpDocumentor\Application;
 use Symfony\Component\Cache\Adapter\AbstractAdapter;
 use Symfony\Component\Cache\Marshaller\DefaultMarshaller;
 use Symfony\Component\Cache\PruneableInterface;
 use Symfony\Component\Cache\Traits\FilesystemTrait;
 
+use function md5;
+use function sprintf;
 use function sys_get_temp_dir;
+
+use const DIRECTORY_SEPARATOR;
 
 final class FilesystemAdapter extends AbstractAdapter implements PruneableInterface
 {
@@ -26,15 +31,17 @@ final class FilesystemAdapter extends AbstractAdapter implements PruneableInterf
         init as doInit;
     }
 
-    private const TTL_ONE_YEAR = 31556926;
+    private const TTL_ONE_MONTH = 2_629_743;
 
-    public function __construct(string $namespace = 'phpdoc', int $defaultLifetime = self::TTL_ONE_YEAR)
+    public function __construct(string $namespace = 'phpdoc', int $defaultLifetime = self::TTL_ONE_MONTH)
     {
         $this->marshaller = new DefaultMarshaller();
 
-        parent::__construct($namespace, $defaultLifetime);
+        parent::__construct($this->prefixNamespaceWithVersion($namespace), $defaultLifetime);
 
-        $this->init($namespace, sys_get_temp_dir() . '/phpdocumentor');
+        $directory = sprintf('%s%s%s', sys_get_temp_dir(), DIRECTORY_SEPARATOR, 'phpdocumentor');
+
+        $this->init($namespace, $directory);
     }
 
     /**
@@ -42,9 +49,28 @@ final class FilesystemAdapter extends AbstractAdapter implements PruneableInterf
      *
      * With phpDocumentor you can set the caching folder in your configuration; this poses an interesting problem with
      * the default FilesystemAdapter of Symfony as that only allows you to set the caching folder upon instantiation.
+     *
+     * This method prefixes the namespace with a hash of the current application version. We do this because changes
+     * between versions, even minor ones, can cause unexpected issues due to serialization. As a trade-off, we thus
+     * force a fresh cache if you use a new/different version of phpDocumentor.
+     *
+     * For example:
+     *
+     *     If a new property is added to a descriptor, upon unserialize PHP will attempt to populate that
+     *     with the default property value, or fail if none is provided, but that default may not be the actual result.
+     *
+     *     Since phpDocumentor runs fast enough for the occasional cache clear, this will have limited impact for end
+     *     users and guarantees that the cached state is correct.
      */
     public function init(string $namespace, string $directory): void
     {
-        $this->doInit($namespace, $directory);
+        $prefixNamespaceWithVersion = $this->prefixNamespaceWithVersion($namespace);
+
+        $this->doInit($prefixNamespaceWithVersion, $directory);
+    }
+
+    private function prefixNamespaceWithVersion(string $namespace): string
+    {
+        return sprintf('%s-%s', md5(Application::VERSION()), $namespace);
     }
 }
