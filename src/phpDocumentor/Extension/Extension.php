4310d46db9ceceef41c62e525249e5bb26df4465
Jaapio <jaap@ijaap.nl>
2023-08-25 13:52:19 +0200

    Feat: add extension base class

--- src/phpDocumentor/Extension/Extension.php
+++ src/phpDocumentor/Extension/Extension.php
@@ -1,8 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace phpDocumentor\Extension;
 
-class Extension
-{
+use Symfony\Component\DependencyInjection\Extension\Extension as BaseExtension;
 
+abstract class Extension extends BaseExtension
+{
+    /**
+     * The constructor of extensions should not be used.
+     *
+     * Extensions are loaded by the {@see ExtensionHandler}. An extension should not apply any logic in its
+     * constructor.
+     */
+    final public function __construct()
+    {
+    }
 }
