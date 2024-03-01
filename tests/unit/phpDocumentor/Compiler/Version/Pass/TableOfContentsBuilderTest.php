1773f94ae5252e6e67c50638049335d79f788171
Mike van Riel <me@mikevanriel.com>
2024-03-01 09:46:51 +0100

    Restructure TableOfContentsBuilder tests to match new structure

--- tests/unit/phpDocumentor/Compiler/Version/Pass/TableOfContentsBuilderTest.php
+++ tests/unit/phpDocumentor/Compiler/Version/Pass/TableOfContentsBuilderTest.php
@@ -13,15 +13,12 @@ declare(strict_types=1);
 
 namespace phpDocumentor\Compiler\Version\Pass;
 
-use phpDocumentor\Descriptor\ApiSetDescriptor;
 use phpDocumentor\Faker\Faker;
 use phpDocumentor\Transformer\Router\Router;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Prophecy\Prophecy\ObjectProphecy;
-
-use function current;
 
 /**
  * @coversDefaultClass \phpDocumentor\Compiler\Version\Pass\TableOfContentsBuilder
@@ -34,18 +31,18 @@ final class TableOfContentsBuilderTest extends TestCase
     use ProphecyTrait;
 
     private Router|ObjectProphecy $router;
-    private TableOfContentsBuilder $pass;
+    private TableOfContentsBuilder $builder;
 
     protected function setUp(): void
     {
         $this->router = $this->givenARouterThatAlwaysReturnsTheFqsen();
-        $this->pass = new TableOfContentsBuilder($this->router->reveal());
+        $this->builder = new TableOfContentsBuilder($this->router->reveal());
     }
 
     /** @covers ::getDescription */
     public function testGetDescription(): void
     {
-        $description = $this->pass->getDescription();
+        $description = $this->builder->getDescription();
 
         $this->assertSame('Builds table of contents for documentation sets', $description);
     }
@@ -53,52 +50,11 @@ final class TableOfContentsBuilderTest extends TestCase
     /** @covers ::__invoke */
     public function testPassingAnythingElseThanAVersionDescriptorIsPassedThroughTransparently(): void
     {
-        $apiDocumentationSet = $this->givenAnApiDocumentationSetWithNamespaces();
+        $apiDocumentationSet = $this->faker()->apiSetDescriptor();
 
-        $result = $this->pass->__invoke($apiDocumentationSet);
+        $this->builder->__invoke($apiDocumentationSet);
 
-        self::assertSame($apiDocumentationSet, $result);
         self::assertEmpty($apiDocumentationSet->getTableOfContents());
-    }
-
-    /** @covers ::__invoke */
-    public function testApiDocumentationSetNamespacesAreAddedToTOC(): void
-    {
-        $apiDocumentationSet = $this->givenAnApiDocumentationSetWithNamespaces();
-        $versionDescriptor = $this->faker()->versionDescriptor([$apiDocumentationSet]);
-
-        $this->pass->__invoke($versionDescriptor);
-
-        self::assertCount(1, $apiDocumentationSet->getTableOfContents());
-
-        $namespacesToc = $apiDocumentationSet->getTableOfContents()->get('Namespaces');
-        self::assertSame('Namespaces', $namespacesToc->getName());
-        self::assertCount(1, $namespacesToc->getRoots());
-
-        $rootEntry = current($namespacesToc->getRoots()->getAll());
-        foreach ($rootEntry->getChildren() as $child) {
-            self::assertSame($rootEntry->getUrl(), $child->getParent());
-        }
-    }
-
-    /** @covers ::__invoke */
-    public function testApiDocumentationSetPackagesAreAddedToTOC(): void
-    {
-        $apiDocumentationSet = $this->givenAnApiDocumentationSetWithPackages();
-        $versionDescriptor = $this->faker()->versionDescriptor([$apiDocumentationSet]);
-
-        $this->pass->__invoke($versionDescriptor);
-
-        self::assertCount(1, $apiDocumentationSet->getTableOfContents());
-
-        $packagesTOC = $apiDocumentationSet->getTableOfContents()->get('Packages');
-        self::assertSame('Packages', $packagesTOC->getName());
-        self::assertCount(1, $packagesTOC->getRoots());
-
-        $rootEntry = current($packagesTOC->getRoots()->getAll());
-        foreach ($rootEntry->getChildren() as $child) {
-            self::assertSame($rootEntry->getUrl(), $child->getParent());
-        }
     }
 
     private function givenARouterThatAlwaysReturnsTheFqsen(): Router|ObjectProphecy
@@ -108,21 +64,5 @@ final class TableOfContentsBuilderTest extends TestCase
             ->will(static fn ($args) => (string) $args[0]->getFullyQualifiedStructuralElementName());
 
         return $router;
-    }
-
-    private function givenAnApiDocumentationSetWithNamespaces(): ApiSetDescriptor
-    {
-        $apiDocumentationSet = $this->faker()->apiSetDescriptor();
-        $apiDocumentationSet->getNamespace()->addChild($this->faker()->namespaceDescriptorTree());
-
-        return $apiDocumentationSet;
-    }
-
-    private function givenAnApiDocumentationSetWithPackages(): ApiSetDescriptor
-    {
-        $apiDocumentationSet = $this->faker()->apiSetDescriptor();
-        $apiDocumentationSet->getPackage()->addChild($this->faker()->namespaceDescriptorTree());
-
-        return $apiDocumentationSet;
     }
 }
